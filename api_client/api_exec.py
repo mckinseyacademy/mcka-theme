c28b0fbf439214e16b6ca2d0dcb5e0d62befa44f
Tivs <tivoli@nurfed.com>
2014-03-24 17:29:09 -0400

    Update card with progress complete

--- api_client/api_exec.py
+++ api_client/api_exec.py
@@ -7,7 +7,6 @@ from django.conf import settings
 AUTH_API = 'api/sessions'
 USER_API = 'api/users'
 
-
 def authenticate(username, password):
     data = {
         "username": username,
@@ -16,15 +15,12 @@ def authenticate(username, password):
     response = POST('{}/{}/'.format(settings.API_SERVER_ADDRESS, AUTH_API), data)
     return JP.from_json(response.read(), models.AuthenticationResponse)
 
-
 def get_user(user_id):
     response = GET('{}/{}/{}'.format(settings.API_SERVER_ADDRESS, USER_API, user_id))
     return JP.from_json(response.read(), models.UserResponse)
 
-
 def delete_session(session_key):
     DELETE('{}/{}/{}'.format(settings.API_SERVER_ADDRESS, AUTH_API, session_key))
-
 
 def register_user(user_hash):
     user_keys = ["username", "first_name", "last_name", "email", "password"]
@@ -32,7 +28,6 @@ def register_user(user_hash):
 
     response = POST('{}/{}/'.format(settings.API_SERVER_ADDRESS, USER_API), data)
     return JP.from_json(response.read())
-
 
 #imports for mocking here
 import json
@@ -46,70 +41,70 @@ def fetch_current_course_for_user(user_id):
                 "name": "Name of Lesson 1",
                 "url": "course/DEF/lesson/ZYX",
                 "is_released": True,
-                "precent_complete": 100,
+                "percent_complete": 100,
             },
             {
                 "name": "Name of Lesson 2",
                 "url": "course/DEF/lesson/YXW",
                 "is_released": True,
-                "precent_complete": 100,
+                "percent_complete": 100,
             },
             {
                 "name": "Name of Lesson 3",
                 "url": "course/DEF/lesson/XWV",
                 "is_released": True,
-                "precent_complete": 80,
+                "percent_complete": 80,
             },
             {
                 "name": "Name of Lesson 4",
                 "url": "course/DEF/lesson/WVU",
                 "is_released": True,
-                "precent_complete": 0,
+                "percent_complete": 0,
             },
             {
                 "name": "Name of Lesson 5",
                 "url": "course/DEF/lesson/VUT",
                 "is_released": True,
-                "precent_complete": 0,
+                "percent_complete": 0,
             },
             {
                 "name": "Name of Lesson 6",
                 "url": "course/DEF/lesson/UTS",
                 "is_released": True,
-                "precent_complete": 0,
+                "percent_complete": 0,
             },
             {
                 "name": "Name of Lesson 7",
                 "url": "course/DEF/lesson/TSR",
                 "is_released": True,
-                "precent_complete": 0,
+                "percent_complete": 0,
             },
             {
                 "name": "Name of Lesson 8",
                 "url": "course/DEF/lesson/SRQ",
                 "is_released": False,
-                "precent_complete": 0,
+                "percent_complete": 0,
             },
             {
                 "name": "Name of Lesson 9",
                 "url": "course/DEF/lesson/RQP",
                 "is_released": False,
-                "precent_complete": 0,
+                "percent_complete": 0,
             },
             {
                 "name": "Name of Lesson 10",
                 "url": "course/DEF/lesson/QPO",
                 "is_released": False,
-                "precent_complete": 0,
+                "percent_complete": 0,
             },
             {
                 "name": "Name of Lesson 11",
                 "url": "course/DEF/lesson/PON",
                 "is_released": False,
-                "precent_complete": 0,
+                "percent_complete": 0,
             },
         ],
-        "last_lesson_index": 2,  
+        "last_lesson_index": 2,
         "last_tab_index": 1,
     }
 
