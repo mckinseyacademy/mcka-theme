4249e2b92c3f7434e7b68119d3e3a127c3696dae
Martyn James <mjames@edx.org>
2014-03-26 16:55:17 -0400

    Pylint updates for remote_auth

--- remote_auth/json_backend.py
+++ remote_auth/json_backend.py
@@ -1,10 +1,14 @@
-from django.conf import settings
-from django.contrib.auth.models import User
+'''
+Backend for our authentication to delegate auth to api request
+Need to implement authenticate, get_user and has_perm
+(as specified in documentation)
+'''
 from django.contrib.auth import get_user_model
 
 from api_client import user_api
 
 
+# pylint: disable=no-member,no-self-use
 class JsonBackend(object):
 
     """
@@ -12,6 +16,9 @@ class JsonBackend(object):
     """
 
     def authenticate(self, username=None, password=None):
+        '''
+        Implements django authenticate that delegates to API
+        '''
         auth_info = user_api.authenticate(username, password)
         user = get_user_model()()
         user.update_response_fields(auth_info.user, auth_info.token)
@@ -19,6 +26,9 @@ class JsonBackend(object):
         return user
 
     def get_user(self, user_id):
+        '''
+        Fetches user model from cache or via API
+        '''
         user = get_user_model().cached_fetch(user_id)
         if None == user:
             user_response = user_api.get_user(user_id)
@@ -27,5 +37,9 @@ class JsonBackend(object):
             user.save()
         return user
 
+# pylint: disable=unused-argument
     def has_perm(self, user_obj, perm, obj=None):
+        '''
+        Does the user have permission to do something
+        '''
         return True
--- remote_auth/models.py
+++ remote_auth/models.py
@@ -1,18 +1,23 @@
+'''
+User model for use with remote authentication; must inherit from AbstractUser
+Don't save in database, but we must inherit from AbstractUser (which in turn
+inherits from model) and therefore tables get contructed
+'''
 from django.db import models
 from django.contrib.auth.models import AbstractUser  # , AbstractBaseUser
-import json_backend
 
 # Create your models here.
 
 
 class RemoteUser(AbstractUser):
+    ''' user object that exists only in cache '''
     # TODO: replace with memcached on server
     temp_user_cache = {}
 
     session_key = models.CharField('session_key', max_length=2000, unique=True)
-    #username = models.CharField('username', max_length = 255, unique = True)
 
     def update_response_fields(self, user_response, session_key=None):
+        ''' take api response and blend the results into this user object '''
         if session_key != None:
             self.session_key = session_key
         self.email = user_response.email
@@ -21,23 +26,30 @@ class RemoteUser(AbstractUser):
         self._image_url = user_response.image_url()
 
     def image_url(self):
+        ''' get image utl for user '''
         return self._image_url
 
     #USERNAME_FIELD = "username"
     #REQUIRED_FIELDS = ["session_key"]
 
     def save(self, **kwargs):
+        '''
+        Notice we only update the cache, not the database
+        The sourse of truth is the system we are talking to va the API
+        '''
         RemoteUser.temp_user_cache[self.id] = self
         return True
 
     @staticmethod
     def cached_fetch(user_id):
+        ''' get from cache if there '''
         if user_id in RemoteUser.temp_user_cache:
             return RemoteUser.temp_user_cache[user_id]
         return None
 
     @staticmethod
     def remove_from_cache(user_id):
+        ''' clean from cache when tearing down session '''
         del RemoteUser.temp_user_cache[user_id]
 
 #    def is_authenticated(self):
