05391e699f68edd31d6a71b82287cafb1f5d5e16
Martyn James <mjames@edx.org>
2014-03-28 12:33:58 -0400

    Program menu using foundation drop downs

--- static/js/application.js
+++ static/js/application.js
@@ -1,30 +1,4 @@
 $(function(){
   /* Javascript to initialise on ready as defined by jquery */
-
-  var toggle_menu = function(container_selector, menu_selector, menu_content_url, fn_callback){
-    var $container = $(container_selector);
-    var $pop_up = $container.find(menu_selector);
-    if($pop_up.length < 1 && menu_content_url){
-      $container.load(menu_content_url);
-    }
-    else{
-      $pop_up.toggle();
-    }
-
-    if(fn_callback)
-      fn_callback();
-  }
-  // Toggle user profile information
-  $('.user-info >.user-name, .user-info >.user-image').on('click', function(){
-    toggle_menu('#profile-container', '.user-profile', '/users/user_profile.html', function(){
-      $('.user-info >.fa').toggleClass('fa-sort-asc fa-sort-desc');
-    });
-  });
-
-  // Toggle course navigation menu
-  $('.program-menu >.fa').on('click', function(){
-    toggle_menu('.program-menu', '.program-menu-content');
-  });
-
 }
 );
