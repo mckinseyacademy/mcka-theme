648d8ec4487821d723d0f743f03b1bda5f6a925f
Martyn James <mjames@edx.org>
2014-03-28 16:46:03 -0400

    user profile drop down as foundation drop down

--- static/js/application.js
+++ static/js/application.js
@@ -1,4 +1,18 @@
 $(function(){
   /* Javascript to initialise on ready as defined by jquery */
+
+  // Load user profile information on demand
+  $('.user-info').one('click', function(){
+    if($('#profile-container .user-profile').length < 1){
+      $('body, .user-info').css('cursor', 'wait');
+      $('#profile-container').load('/users/user_profile.html', function(){
+        $('body, .user-info').css('cursor', 'inherit');
+      });
+    }
+  });
+
+  $('#profile-container').on('closed opened', function(){
+    $('.user-info >.fa').toggleClass('fa-sort-asc fa-sort-desc');
+  })
 }
 );
