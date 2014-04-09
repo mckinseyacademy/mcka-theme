00bddbc04980226ae9206dc189f8d913cf227ebd
Andy Parsons <andyparsons@gmail.com>
2014-04-09 13:11:57 -0400

    PEP8 indentation

--- marketing/views.py
+++ marketing/views.py
@@ -1,8 +1,7 @@
 ''' rendering templates from requests related to marketing '''
 from django.shortcuts import render
 
-# Create your views here.
 
 def infer_default_navigation(request, page_name):
-  page = "marketing/{0}.haml".format(page_name)
-  return render(request, page)
+    page = "marketing/{0}.haml".format(page_name)
+    return render(request, page)
