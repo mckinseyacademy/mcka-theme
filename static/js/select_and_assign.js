66413ef090be48fc93937e03e061a732c09b9c20
Martyn James <mjames@edx.org>
2014-04-28 14:18:18 -0400

    Genericise List Assignment where used

---
+++ static/js/select_and_assign.js
@@ -0,0 +1,68 @@
+function enable_selection(selections, activator){
+  var _multi_selection = function(){
+    $(this).toggleClass('selected');
+  };
+
+  var _default_success = function(response){
+    alert(response.message);
+  };
+
+  var _default_error = function(jqXHR, textStatus, errorThrown){
+    alert(textStatus + " - " + errorThrown);
+  };
+
+  if(!$.isArray(selections)){
+    selections = [selections];
+  }
+
+  for (var i = 0; i < selections.length; ++i){
+    var select_func = _multi_selection;
+    var select_class_name = selections[i].class_name ? selections[i].class_name : "selected";
+    var select_action = selections[i].select_action ? selections[i].select_action : "click";
+    if(selections[i].single_select){
+      var use_selector = selections[i].selector;
+      select_func = function(){
+        $(use_selector).removeClass(select_class_name);
+        $(this).addClass(select_class_name);
+      };
+    }
+    $(selections[i].selector).on(select_action, select_func);
+  };
+
+  $(activator.selector).on('click', function(event){
+    event.preventDefault();
+
+    var data = {};
+    for (var i = 0; i < selections.length; ++i){
+      var min_count = selections[i].minimum_count ? selections[i].minimum_count : 1;
+      var data_selector = selections[i].data_selector ? selections[i].data_selector : "";
+      var these_selections = $(selections[i].selector + "." + select_class_name + data_selector);
+      if(these_selections.length < min_count){
+        alert(selections[i].minimum_count_message);
+        return false;
+      }
+
+      var selection_data = [];
+      these_selections.each(function(index, include_value){
+        selection_data.push(include_value.id);
+      });
+      data[selections[i].submit_name] = (selections[i].single_select && data_selector.length == 0) ? selection_data[0] : selection_data;
+    };
+
+    var on_success = activator.success ? activator.success : _default_success;
+    var on_error = activator.error ? activator.error : _default_error;
+    var use_url = activator.submit_url ? activator.submit_url : $(this).attr('href');
+
+    data["csrfmiddlewaretoken"] = $.cookie('apros_csrftoken');
+    $.ajax({
+      url: use_url,
+      type: "POST",
+      data: data,
+      dataType: 'json',
+      success: on_success,
+      error: on_error
+    });
+
+    return false;
+  });
+}
