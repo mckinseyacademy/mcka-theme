4b6ab1818b54c96c2af9d7c1fe881a6a0934a46d
Andy Parsons <andyparsons@gmail.com>
2014-05-13 18:02:15 -0400

    terms and privacy pages added

--- mcka_apros/urls.py
+++ mcka_apros/urls.py
@@ -4,6 +4,8 @@ from django.conf import settings
 urlpatterns = patterns(
     '',
     url(r'^$', 'accounts.views.home', name='home'),
+    url(r'^terms/', 'main.views.terms', name='terms'),
+    url(r'^privacy/', 'main.views.privacy', name='privacy'),
     url(r'^accounts/', include('accounts.urls'), name='accounts'),
     url(r'^courses/', include('courses.urls'), name='courses'),
     url(r'^admin/', include('admin.urls'), name='admin'),
