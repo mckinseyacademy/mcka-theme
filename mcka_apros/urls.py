ce4643a27f3a84b5ac649bf7a80eb688e5ad9580
Andy Parsons <andyparsons@gmail.com>
2014-05-14 12:46:31 -0400

    placeholder FAQ added

--- mcka_apros/urls.py
+++ mcka_apros/urls.py
@@ -6,6 +6,7 @@ urlpatterns = patterns(
     url(r'^$', 'accounts.views.home', name='home'),
     url(r'^terms/', 'main.views.terms', name='terms'),
     url(r'^privacy/', 'main.views.privacy', name='privacy'),
+    url(r'^faq/', 'main.views.faq', name='faq'),
     url(r'^accounts/', include('accounts.urls'), name='accounts'),
     url(r'^courses/', include('courses.urls'), name='courses'),
     url(r'^admin/', include('admin.urls'), name='admin'),
