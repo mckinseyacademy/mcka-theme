69f12fdcd94a1f06bcef2f9ef8d6401ce5e8fc6a
Andy Parsons <andyparsons@gmail.com>
2014-05-21 16:00:26 -0400

    include course_id for course homepage queries

--- courses/views.py
+++ courses/views.py
@@ -68,11 +68,11 @@ def course_landing_page(request, course_id):
 
     data = {
         "user": request.user,
-        "articles": CuratedContentItem.objects.filter(content_type=CuratedContentItem.ARTICLE).order_by('sequence')[:3],
-        "videos": CuratedContentItem.objects.filter(content_type=CuratedContentItem.VIDEO).order_by('sequence')[:3],
-        "tweet": CuratedContentItem.objects.filter(content_type=CuratedContentItem.TWEET).order_by('sequence').last(),
-        "quote": CuratedContentItem.objects.filter(content_type=CuratedContentItem.QUOTE).order_by('sequence').last(),
-        "infographic": CuratedContentItem.objects.filter(content_type=CuratedContentItem.IMAGE).order_by('sequence').last(),
+        "articles": CuratedContentItem.objects.filter(course_id=course_id, content_type=CuratedContentItem.ARTICLE).order_by('sequence')[:3],
+        "videos": CuratedContentItem.objects.filter(course_id=course_id, content_type=CuratedContentItem.VIDEO).order_by('sequence')[:3],
+        "tweet": CuratedContentItem.objects.filter(course_id=course_id, content_type=CuratedContentItem.TWEET).order_by('sequence').last(),
+        "quote": CuratedContentItem.objects.filter(course_id=course_id, content_type=CuratedContentItem.QUOTE).order_by('sequence').last(),
+        "infographic": CuratedContentItem.objects.filter(course_id=course_id, content_type=CuratedContentItem.IMAGE).order_by('sequence').last(),
     }
     return render(request, 'courses/course_main.haml', data)
 
