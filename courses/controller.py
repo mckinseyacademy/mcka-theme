faf27045bc70cecb210384f31ca2da7a0b12f1f7
Martyn James <mjames@edx.org>
2014-05-27 16:52:01 -0400

    Updated code a little to simplify

--- courses/controller.py
+++ courses/controller.py
@@ -70,12 +70,13 @@ def build_page_info_for_course(
                     page.prev_url = prev_page.navigation_url
                     prev_page.next_url = page.navigation_url
                 prev_page = page
-            if current_page == sequential.pages[-1] and chapter != course.chapters[-1]:
-                current_page.next_lesson_link = True
 
     if not current_page:
         current_sequential = current_chapter.sequentials[0] if len(current_chapter.sequentials) > 0 else None
         current_page = current_sequential.pages[0] if current_sequential and len(current_sequential.pages) > 0 else None
+
+    if current_sequential == current_chapter.sequentials[-1] and current_page == current_sequential.pages[-1] and current_chapter != course.chapters[-1]:
+        current_page.next_lesson_link = True
 
     return course, current_chapter, current_sequential, current_page
 
@@ -227,7 +228,7 @@ def group_project_location(user_id, course, sequential_id=None):
             project_groups = course_api.get_course_content_groups(course.id, project.id)
         except:
             project_groups = []
-            
+
         intersection_ids = [pg.group_id for pg in project_groups if pg.group_id in user_project_group_ids]
         if len(intersection_ids) > 0:
             group_project = project
