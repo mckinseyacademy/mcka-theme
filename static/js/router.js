4c59179233b876e1428a75586f00f982a7ca73a4
Tivs <tivoli@nurfed.com>
2014-05-29 10:37:43 -0400

    Delivers MCKIN-1290 Replace overview video with static image

--- static/js/router.js
+++ static/js/router.js
@@ -27,9 +27,6 @@ var Router = Backbone.Router.extend({
   },
 
   course_overview: function(course_id) {
-    console.log(OO);
-    console.log('asdfasdf');
-    OO.Player.create('mk_overview_player', 'o3bHd4bTq6tVR5KxP8m1RXDl9vpVaNMA', {width: '100%', height: '100%'});
   },
 
   course_cohort: function(course_id) {
