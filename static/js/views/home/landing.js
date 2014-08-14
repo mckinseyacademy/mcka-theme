e0e16c6e055ddd70ed4b14b445dd30d37107a687
Dino Cikatic <dcikatic@edx.org>
2014-08-14 14:18:49 +0200

    MCKIN-1650 fix popup location on homepage

--- static/js/views/home/landing.js
+++ static/js/views/home/landing.js
@@ -10,10 +10,17 @@ Apros.views.HomeLanding = Backbone.View.extend({
         t_width     = tagline.width(),
         t_offset    = tagline.offset();
 
+    var render_warning_popup = function(){
+      if($('#generalModal input.show-modal').length > 0){
+        $('html,body').css('width', '1800px');
+        setTimeout($('#generalModal').foundation('reveal', 'open'), 1000);
+      }
+    }
+
     $('html,body').animate({
         scrollTop: t_offset.top - ($window.height() - tagline.outerHeight(true)) / 2,
         scrollLeft: t_offset.left - ($window.width() - tagline.outerWidth(true)) / 2
-    }, 0);
+    }, 0, render_warning_popup());
   },
 
   render: function() {
