2786ed250d3038f2d8bc1da3f142f0a8d8f7ad09
Dino Cikatic <dcikatic@edx.org>
2014-08-22 10:30:57 +0200

    MIgrations fixes

--- admin/migrations/0001_initial.py
+++ admin/migrations/0001_initial.py
@@ -8,8 +8,16 @@ from django.db import models
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
+        # Adding model 'UserRegistrationError'
+        db.create_table(u'admin_userregistrationerror', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('task_key', self.gf('django.db.models.fields.CharField')(max_length=40, db_index=True)),
+            ('error', self.gf('django.db.models.fields.TextField')(default='')),
+        ))
+        db.send_create_signal(u'admin', ['UserRegistrationError'])
+
         # Adding model 'UserRegistrationBatch'
-        db.create_table(u'admin_userregistration_batch', (
+        db.create_table(u'admin_userregistrationbatch', (
             (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('task_key', self.gf('django.db.models.fields.CharField')(unique=True, max_length=40, db_index=True)),
             ('attempted', self.gf('django.db.models.fields.IntegerField')(default=0)),
@@ -19,36 +27,68 @@ class Migration(SchemaMigration):
         ))
         db.send_create_signal(u'admin', ['UserRegistrationBatch'])
 
-        db.create_table(u'admin_userregistration_error', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('task_key', self.gf('django.db.models.fields.CharField')(unique=False, max_length=40, db_index=True)),
-            ('attempted', self.gf('django.db.models.fields.TextField')(default='')),
-        ))
-        db.send_create_signal(u'admin', ['UserRegistrationError'])
-
 
     def backwards(self, orm):
+        # Deleting model 'UserRegistrationError'
+        db.delete_table(u'admin_userregistrationerror')
+
         # Deleting model 'UserRegistrationBatch'
-        db.delete_table(u'admin_userregistration_batch')
-        db.delete_table(u'admin_userregistration_error')
+        db.delete_table(u'admin_userregistrationbatch')
+
 
     models = {
-        u'admin.userregistration_batch': {
-            'Meta': {'object_name': 'UserRegistrationBatch'},
+        u'accounts.remoteuser': {
+            'Meta': {'object_name': 'RemoteUser'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Group']"}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'task_key': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '40', 'db_index': 'True'}),
-            'time_requested': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'session_key': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Permission']"}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        u'admin.userregistrationbatch': {
+            'Meta': {'object_name': 'UserRegistrationBatch'},
             'attempted': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
             'failed': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
-            'succeded': ('django.db.models.fields.IntegerField', [], {'default': '0'})
-        },
-
-        u'admin.userregistration_error': {
-            'Meta': {'object_name': 'UserRegistrationError'},
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'task_key': ('django.db.models.fields.CharField', [], {'unique': 'False', 'max_length': '40', 'db_index': 'True'}),
-            'error': ('django.db.models.fields.TextField', [], {'default': ''})
+            'succeded': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            'task_key': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '40', 'db_index': 'True'}),
+            'time_requested': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'})
         },
+        u'admin.userregistrationerror': {
+            'Meta': {'object_name': 'UserRegistrationError'},
+            'error': ('django.db.models.fields.TextField', [], {'default': "''"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'task_key': ('django.db.models.fields.CharField', [], {'max_length': '40', 'db_index': 'True'})
+        },
+        u'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        u'auth.permission': {
+            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        }
     }
 
     complete_apps = ['admin']
