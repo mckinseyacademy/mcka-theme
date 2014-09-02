4009bc6279c0032283d04e4c7ba41850fdde48b9
Dino Cikatic <dcikatic@edx.org>
2014-09-02 13:30:40 +0200

    MCKIN-2028 Switch to CSS3Pie

--- static/js/views/home/courses.js
+++ static/js/views/home/courses.js
@@ -51,53 +51,5 @@ Apros.views.HomeCourses = Backbone.View.extend({
     this.btn_right.toggleClass('disabled', last_in_view);
     var myNav = navigator.userAgent.toLowerCase();
     var browserVersion = (myNav.indexOf('msie') != -1) ? parseInt(myNav.split('msie')[1]) : false;
-    if ($.checkGradients()){
-      this.proficiencyIEfix();
-    }
-  },
-  proficiencyIEfix: function(){
-    var classes = $(".course-proficiency .visualization").attr('class').split(/\s+/);
-    var percentage = 0;
-    $.each(classes, function(index, item){
-      if(item.indexOf('progress-') == 0){
-        percentage = item.split('progress-')[1];
-      }
-    });
-    var width = 100,
-        height = 100,
-        radius = Math.min(width, height) / 2;
-
-    var color = d3.scale.ordinal()
-        .range(["#333333", "#b1c2cc", "#7b6888"]);
-
-    var arc = d3.svg.arc()
-        .outerRadius(radius)
-        .innerRadius(radius - 15);
-
-    var pie = d3.layout.pie()
-        .sort(null)
-        .value(function(d) { return d.population; });
-
-    $('.course-proficiency .progress-radial').remove();
-    var svg = d3.select(".course-proficiency .visualization").append("svg")
-        .attr("width", width)
-        .attr("height", height)
-      .append("g")
-        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
-
-    var data=[{'population': percentage}, {'population': (100 - percentage)}];
-
-    data.forEach(function(d) {
-      d.population = +d.population;
-    });
-
-    var g = svg.selectAll(".arc")
-        .data(pie(data))
-      .enter().append("g")
-        .attr("class", "arc");
-
-    g.append("path")
-        .attr("d", arc)
-        .style("fill", function(d) { return color(d.value); });
   }
 });
