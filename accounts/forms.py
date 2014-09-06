e00c3e4ffdb1acbc4cc4a985f6f17d7bfa97b08a
Andy Parsons <andyparsons@gmail.com>
2014-09-06 18:49:02 -0400

    cosmetic change for MCKIN-2298

--- accounts/forms.py
+++ accounts/forms.py
@@ -328,7 +328,8 @@ class ActivationForm(NoSuffixLabelForm):
     #last_name = forms.CharField(max_length=255)
 
     email = forms.CharField(max_length=255, widget = forms.TextInput(attrs={'readonly':'readonly'}), label=mark_safe('E-mail'))
-    password = forms.CharField(widget=forms.PasswordInput(), label=mark_safe('Password (must be 8 or more characters and contain at least 2 of each of the following character categories: uppercase, lowercase, numeric, special characters like !, @, $) <span class="required-field"></span>'))
+    password = forms.CharField(widget=forms.PasswordInput(), 
+        label=mark_safe('Password <span class="required-field"></span> <span class="tip">Must be at least 8 characters and include upper and lowercase letters - plus numbers OR special characters.</span>'))
     #confirm_password = forms.CharField(widget=forms.PasswordInput())
     username = forms.CharField(widget=UserNameInput(attrs={'required': True}), initial='', max_length=255, label=mark_safe('Public username <span class="required-field"></span>'))
     company = forms.CharField(max_length=255, required=False)
