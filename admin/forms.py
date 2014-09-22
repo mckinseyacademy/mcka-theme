bee513101fb40a50bffba7ab73f8713d3dac80c9
Davorin Sego <davorin.sego@gmail.com>
2014-09-22 13:53:49 +0200

    changed to course_id per Dino’s comment

--- admin/forms.py
+++ admin/forms.py
@@ -96,7 +96,7 @@ class PermissionForm(forms.Form):
         for course in courses:
             self.fields[course.id] = forms.MultipleChoiceField(
                 required=False,
-                label="{} ({})".format(course.name, course.number),
+                label="{} ({})".format(course.name, course.id),
                 widget=forms.CheckboxSelectMultiple,
                 choices=self.available_roles()
             )
