4a46529bb6349d358f71394726387a6a66762009
Davorin Sego <davorin.sego@gmail.com>
2014-09-30 11:59:13 +0200

    remove ‘slashes’ from course id

--- admin/forms.py
+++ admin/forms.py
@@ -96,7 +96,7 @@ class PermissionForm(forms.Form):
         for course in courses:
             self.fields[course.id] = forms.MultipleChoiceField(
                 required=False,
-                label="{} ({})".format(course.name, course.id),
+                label="{} ({})".format(course.name, course.id.replace('slashes:', '')),
                 widget=forms.CheckboxSelectMultiple,
                 choices=self.available_roles()
             )
