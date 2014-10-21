f62cc11cdf81ee5fc9306653cdeaead4b36a9f07
Tivs <tivoli@nurfed.com>
2014-10-21 15:46:35 -0400

    Fixes MCKIN-2500 Single responses from mapbox are not arrays

--- static/js/collections/cohort_cities.js
+++ static/js/collections/cohort_cities.js
@@ -3,13 +3,14 @@ Apros.collections.CohortCities = Backbone.Collection.extend({
 
   url: function() {
     var cityList = _(CohortMapCities).reduce(function(list, city){
-      var prefix = list.length ? ';' : ''
+      var prefix = list.length ? ';' : '';
       return list += prefix + city.city;
     }, '');
     return 'https://api.tiles.mapbox.com/v4/geocode/mapbox.places-v1/' + cityList + '.json?access_token=' + L.mapbox.accessToken;
   },
 
   parse: function(response) {
+    response = _(response).isArray() ? response : [response];
     var cities = _(response).map(function(city) {
       return _(city.features).find(function(feature) {
         return /^city/.test(feature.id);
