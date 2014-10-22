562f8482d3062cd982367875ee778cfe7391a433
Davorin Sego <davorin.sego@gmail.com>
2014-10-22 12:51:16 +0200

    display weeks on chart x-axis

--- static/js/views/admin/analytics.js
+++ static/js/views/admin/analytics.js
@@ -75,22 +75,24 @@ Apros.views.AdminAnalyticsParticipantActivity = Backbone.View.extend({
               .margin({top: 30, right: 60, bottom: 50, left: 70})
               //We can set x data accessor to use index. Reason? So the bars all appear evenly spaced.
               .x(function(d,i) { return i })
-              .y(function(d,i) {return d[1] })
-              .tooltips(false)
-              ;
+              .y(function(d,i) { return d[1] })
+              .tooltips(false);
 
-        chart.xAxis.tickFormat(function(d) {
-          var dx = dataJson[0].values[d] && dataJson[0].values[d][0] || 0;
-          return d3.time.format('%x')(new Date(dx))
+        var weeks = d3.range(0, dataJson[0].values.length, 7)
+        chart.xAxis.tickValues(weeks).tickFormat(function(d) {
+          return 1 + Math.floor(d / 7);
         });
 
-        chart.bars.forceY([0]);
 
         d3.select(_this.el)
           .datum(dataJson)
           .transition()
           .duration(0)
           .call(chart);
+
+        d3.select(_this.el)
+          .selectAll('.nv-x .nv-axisMaxMin:last-child>text')
+          .text('weeks')
 
         d3.select(_this.el)
           .append("text")
