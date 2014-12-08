df1ce4426274cd8aca8f42ed98f148195143f72e
Dino Cikatic <dcikatic@edx.org>
2014-12-08 13:41:26 +0100

    MCKIN-2826 re-set chart width and color

--- static/js/views/admin/analytics.js
+++ static/js/views/admin/analytics.js
@@ -16,7 +16,7 @@ Apros.views.AdminAnalyticsProgress = Backbone.View.extend({
                     return [value];
                 });
     nv.addGraph(function() {
-      var width = 750, height = 350;
+      var width = 775, height = 350;
       var chart = nv.models.cumulativeLineChart()
                     .x(function(d) { return d[0] })
                     .y(function(d) { return Math.ceil(d[1])/100 }) //adjusting, 100% is 1.00, not 100 as it is in the data
@@ -51,7 +51,7 @@ Apros.views.AdminAnalyticsProgress = Backbone.View.extend({
           .append("text")
           .text(function(){return daysNumber > 7 ? 'weeks': 'days';})
           .attr('y', 15)
-          .attr('x', 5);
+          .attr('x', 10);
 
       return chart;
     });
@@ -83,11 +83,13 @@ Apros.views.AdminAnalyticsParticipantActivity = Backbone.View.extend({
     dataJson[0].values.unshift([0,0]);
 
     nv.addGraph(function() {
+        var width = 805, height = 350;
         var chart = nv.models.linePlusBarChart()
               .margin({top: 30, right: 60, bottom: 50, left: 70})
               //We can set x data accessor to use index. Reason? So the bars all appear evenly spaced.
               .x(function(d,i) { return i })
               .y(function(d,i) { return d[1] })
+              .width(width).height(height)
               .tooltips(false);
 
         var weeks = d3.range(0, dataJson[0].values.length, 7)
@@ -128,7 +130,7 @@ Apros.views.AdminAnalyticsParticipantActivity = Backbone.View.extend({
           .text("PARTICIPANTS")
           .attr("transform", "rotate(-90)")
           .attr("x", -220)
-          .attr("y",910)
+          .attr("y",790)
           .attr("dy", ".35em")
           ;
 
