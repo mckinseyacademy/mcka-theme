7de166ea71c791cdfdacd41f24784a2ef3116e22
Jonathan Piacenti <kelketek@gmail.com>
2015-06-10 15:44:01 +0000

    Allow internal admins to upload company photos.

--- admin/views.py
+++ admin/views.py
@@ -1520,7 +1520,11 @@ def load_group_projects_info_for_course(course, companies):
 def not_authorized(request):
     return render(request, 'admin/not_authorized.haml')
 
-@permission_group_required(PERMISSION_GROUPS.MCKA_ADMIN, PERMISSION_GROUPS.CLIENT_ADMIN)
+@permission_group_required(
+    PERMISSION_GROUPS.MCKA_ADMIN, PERMISSION_GROUPS.CLIENT_ADMIN,
+    PERMISSION_GROUPS.INTERNAL_ADMIN
+)
+@client_admin_access
 def change_company_image(request, client_id='new', template='change_company_image', error=None, company_image=None):
     ''' handles requests for login form and their submission '''
     if(client_id == 'new' and not company_image):
@@ -1549,7 +1553,11 @@ def change_company_image(request, client_id='new', template='change_company_imag
         data
     )
 
-@permission_group_required(PERMISSION_GROUPS.MCKA_ADMIN, PERMISSION_GROUPS.CLIENT_ADMIN)
+@permission_group_required(
+    PERMISSION_GROUPS.MCKA_ADMIN, PERMISSION_GROUPS.CLIENT_ADMIN,
+    PERMISSION_GROUPS.INTERNAL_ADMIN
+)
+@client_admin_access
 def company_image_edit(request, client_id="new"):
     if request.method == 'POST':
         heightPosition = request.POST.get('height-position')
@@ -1598,7 +1606,10 @@ def company_image_edit(request, client_id="new"):
             client.update_and_fetch(client.id,  {'logo_url': '/accounts/' + new_image_url})
             return HttpResponse(json.dumps({'image_url': '/accounts/' + new_image_url, 'client_id': client.id}), content_type="application/json")
 
-@permission_group_required(PERMISSION_GROUPS.MCKA_ADMIN, PERMISSION_GROUPS.CLIENT_ADMIN)
+@permission_group_required(
+    PERMISSION_GROUPS.MCKA_ADMIN, PERMISSION_GROUPS.CLIENT_ADMIN,
+    PERMISSION_GROUPS.INTERNAL_ADMIN
+)
 def upload_company_image(request, client_id='new'):
     ''' handles requests for login form and their submission '''
     error = None
