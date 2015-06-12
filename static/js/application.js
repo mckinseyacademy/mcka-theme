d5207abff3264f65375bf323efe3bf23d776e10e
Braden MacDonald <braden@opencraft.com>
2015-06-12 00:22:39 -0700

    Fix course discussion navigation in IE 9

--- static/js/application.js
+++ static/js/application.js
@@ -12,7 +12,7 @@ window.Apros = {
         $(this).addClass('selected');
       }
     });
-    Backbone.history.start({pushState: has_push, hashChange: false});
+    Backbone.history.start({pushState: has_push, hashChange: !has_push});
   },
 
   jumpLinkRewriter: function(jump_link){
