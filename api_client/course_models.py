2b98c07b719c365baa2cdf1585b225f0664bd9d0
Tivs <tivoli@nurfed.com>
2015-10-09 11:07:59 -0700

    Fixes MCKIN-3574 Assesment score not always an attribute

--- api_client/course_models.py
+++ api_client/course_models.py
@@ -364,7 +364,7 @@ class Course(CategorisedJsonObject):
             "modules": [],
             "group_activities": [],
         }
-        graded_lessons = [lesson for lesson in self.chapters if lesson.assesment_score != None]
+        graded_lessons = [lesson for lesson in self.chapters if getattr(lesson, 'assesment_score', None) != None]
         for lesson in graded_lessons:
             for sequential in lesson.sequentials:
                 if sequential.name.find('Assessment') != -1:
