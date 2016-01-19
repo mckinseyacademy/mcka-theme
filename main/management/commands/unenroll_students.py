3007f86d5baaa8f11cb18286d8c225ce9fd2067d
Tivs <tivoli@nurfed.com>
2016-01-19 10:47:06 -0700

    Delivers MCKIN-3726 Updated unenroll list for course and add file path as the argument for unenroll management command

--- main/management/commands/unenroll_students.py
+++ main/management/commands/unenroll_students.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 from django.core.management.base import BaseCommand
 from django.conf import settings
@@ -11,10 +12,14 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         unenroll_msg = "Unenrolling students..."
-
         self.stdout.write(unenroll_msg)
+        if len(args) == 0:
+            raise CommandError("A file path must be specified")
+        filename = args[0]
+        if not os.path.isfile(filename):
+            raise CommandError("File does not exist at the specified path.")
 
-        with open('main/fixtures/unenroll_list.json') as json_file:
+        with open(filename) as json_file:
             course_list = json.load(json_file)
             for course in course_list:
 
