4cbae1df7afef141b7ce0478e4dd5177c0b59cbc
E. Kolpakov <eugeny.kolpakov@gmail.com>
2016-02-11 12:20:22 +0300

    Tests for json_backend.authenticate

--- accounts/tests.py
+++ accounts/tests.py
@@ -7,12 +7,14 @@ import mock
 
 from accounts.controller import ProcessAccessKeyResult, process_access_key, AssignStudentToProgramResult, \
     EnrollStudentInCourseResult, enroll_student_in_course
+from accounts.json_backend import JsonBackend
 from api_client.api_error import ApiError
 from api_client.course_models import CourseListCourse
 from api_client.organization_models import Organization
+from api_client.user_models import AuthenticationResponse, UserResponse
 from .forms import ActivationForm, FinalizeRegistrationForm
 from .models import UserActivation, RemoteUser
-from .views import access_key, MISSING_ACCESS_KEY_ERROR, _cleanup_username as cleanup_username
+from .views import access_key, MISSING_ACCESS_KEY_ERROR, _cleanup_username as cleanup_username, login
 from admin.models import AccessKey, ClientCustomization, Program
 from mock import patch, Mock
 import uuid
@@ -31,7 +33,7 @@ class ApplyPatchMixin(object):
 
 
 def _make_user(username='johndoe', email='john@doe.org', password='password'):
-        return RemoteUser.objects.create_user(username=username, email=email, password=password)
+    return RemoteUser.objects.create_user(username=username, email=email, password=password)
 
 
 def _make_company(org_id, display_name='company_name'):
@@ -88,6 +90,110 @@ class AccountsFormsTests(TestCase):
         # But they can override 'initial' data
         self.assertEqual(form.cleaned_data['username'], 'custom')
         self.assertEqual(form.cleaned_data['city'], 'Testerville')
+
+
+@ddt.ddt
+class JsonBackendTests(TestCase, ApplyPatchMixin):
+    def setUp(self):
+        self.backend = JsonBackend()
+        self.user_api = self.apply_patch('accounts.json_backend.user_api')
+
+    def _make_auth_response(self, user, token='123'):
+        result = mock.Mock(spec=AuthenticationResponse)
+        result.user = user
+        result.token = token
+        return result
+
+    def _make_user_response(self, user):
+        result = UserResponse(dictionary=user.__dict__)
+        return result
+
+    def test_authenticate_username_and_password_existing_user(self):
+        existing_user = _make_user()
+        self.user_api.authenticate.return_value = self._make_auth_response(existing_user)
+        self.user_api.get_user.return_value = self._make_user_response(existing_user)
+
+        auth_response = self.backend.authenticate(existing_user.username, existing_user.password)
+        self.user_api.authenticate.assert_called_once_with(
+            existing_user.username, existing_user.password, remote_session_key=None
+        )
+        self.assertIsNotNone(auth_response)
+        self.assertEqual(auth_response.username, existing_user.username)
+
+    @ddt.data(
+        ('username', None),
+        (None, 'password')
+    )
+    @ddt.unpack
+    def test_authenticate_missing_username_or_password(self, username, password):
+        existing_user = _make_user(username='username', password='password')
+        self.user_api.authenticate.return_value = self._make_auth_response(existing_user)
+        self.user_api.get_user.return_value = self._make_user_response(existing_user)
+
+        auth_response = self.backend.authenticate(username, password)
+        self.user_api.authenticate.assert_called_once_with(username, password, remote_session_key=None)
+        self.assertIsNotNone(auth_response)
+        self.assertEqual(auth_response.username, existing_user.username)
+
+    @ddt.data(
+        ('key1', ['key1'], False),
+        ('key1', ['key2'], True),
+    )
+    @ddt.unpack
+    def test_authenticate_remote_session_key(self, session_key, existing_session_keys, raises):
+        existing_user = _make_user()
+
+        def _get_session(remote_session_key):
+            if remote_session_key not in existing_session_keys:
+                http_error = HTTPError("http://irrelevant", 404, "Session not found", None, None)
+                raise ApiError(http_error, "get_session", None)
+            return mock.Mock(user_id=existing_user.id)
+
+        self.user_api.authenticate.return_value = self._make_auth_response(existing_user)
+        self.user_api.get_session.side_effect = _get_session
+        self.user_api.get_user.return_value = self._make_user_response(existing_user)
+
+        auth_response = self.backend.authenticate(remote_session_key=session_key)
+        self.user_api.authenticate.assert_not_called()
+        if raises:
+            self.assertIsNone(auth_response)
+        else:
+            self.assertIsNotNone(auth_response)
+            self.assertEqual(auth_response.username, existing_user.username)
+
+    def test_authenticate_remote_session_key_username_and_password(self):
+        existing_user = _make_user()
+
+        def _authenticate(username, password, remote_session_key=None):
+            if username == existing_user.username and password == existing_user.password:
+                return self._make_auth_response(existing_user)
+            else:
+                http_error = HTTPError("http://irrelevant", 404, "User not found", None, None)
+                raise ApiError(http_error, "authenticate", None)
+
+        self.user_api.authenticate.side_effect = _authenticate
+        self.user_api.get_user.return_value = self._make_user_response(existing_user)
+
+        auth_response = self.backend.authenticate(
+            existing_user.username, existing_user.password, remote_session_key="session_key"
+        )
+        self.user_api.get_session.assert_not_called()
+        self.user_api.authenticate.assert_called_once_with(
+            existing_user.username, existing_user.password, remote_session_key="session_key"
+        )
+        self.assertIsNotNone(auth_response)
+        self.assertEqual(auth_response.username, existing_user.username)
+
+    def test_authenticate_remote_session_key_none(self):
+        existing_user = _make_user()
+
+        self.user_api.authenticate.return_value = self._make_auth_response(existing_user)
+        self.user_api.get_session.return_value = mock.Mock(user_id=existing_user.id)
+        self.user_api.get_user.return_value = self._make_user_response(existing_user)
+
+        self.backend.authenticate(remote_session_key=None)
+        self.user_api.authenticate.assert_called_once_with(None, None, remote_session_key=None)
+        self.user_api.get_session.assert_not_called()
 
 
 class TestUserObject(object):
