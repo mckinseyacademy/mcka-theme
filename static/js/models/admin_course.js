bf4934712257665614d28dac406f05876bb9f989
Frano Sincic <franosincic@Franos-MacBook-Pro.local>
2016-02-15 14:21:16 +0100

    Course list MCKIN-3744

--- static/js/models/admin_course.js
+++ static/js/models/admin_course.js
@@ -1,1 +1,5 @@
+<<<<<<< b76d42e58bb6b8f08063669e8035c33a5c7e847d
   Apros.models.AdminCourse = Backbone.Model.extend({});
+=======
+	Apros.models.AdminCourse = Backbone.Model.extend({});
+>>>>>>> Course list MCKIN-3744
