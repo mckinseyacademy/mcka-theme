3a6f4f3eb5553f079c90321ed78a0750e9e995a0
Josip Kotarac <jkotarac@extensionengine.com>
2016-03-15 11:36:42 +0000

    MCKIN-3828

--- static/js/views/admin/course_details.js
+++ static/js/views/admin/course_details.js
@@ -1,7 +1,9 @@
   Apros.views.CourseDetailsView = Backbone.View.extend({
     initialize: function(){
+      _this = this;
       this.collection.fetch({success:function(collection, response, options)
         {
+          _this.updateColumns();
           collection.getSlowFetchedStatus = true;
           collection.slowFieldsSuccess(collection, response, options);
         }});
@@ -46,12 +48,10 @@
           {
             return '<i class="fa fa-spinner fa-spin"></i>';
           }
-          progress = attributes['progress']
-          if (progress[0] != '0')
-            return '' + progress + '%'; 
-          if (progress[1] != '0')
-            return '' + progress[1] + progress[2] + '%'; 
-          return '' + progress[2] + '%'; 
+          value = attributes['progress']
+          if (value == '-')
+            return value;
+          return '' + parseInt(value) + '%'; 
         }},
         { title: 'Proficiency', index: true, name: 'proficiency', actions: function(id, attributes) 
         { 
@@ -59,15 +59,14 @@
           {
             return '<i class="fa fa-spinner fa-spin"></i>';
           }
-          proficiency = attributes['proficiency']
-          if (proficiency[0] != '0')
-            return '' + proficiency + '%'; 
-          if (proficiency[1] != '0')
-            return '' + proficiency[1] + proficiency[2] + '%'; 
-          return '' + proficiency[2] + '%'; 
+          value = attributes['proficiency']
+          if (value == '-')
+            return value;
+          return '' + parseInt(value) + '%'; 
         }}
         ]
       });
+      
       coursesListDetailsViewGrid['partial_collection'] = this.collection;
       this.$el.scroll(this.fetchPages);
     },
@@ -83,5 +82,63 @@
           }
         }});
       }
-    }
+    },
+    updateColumns: function()
+    {
+      var assessment_template = { title: '', index: true, name: '', actions: function(id, attributes) 
+        { 
+          if (attributes['grade'] == '.')
+          {
+            return '<i class="fa fa-spinner fa-spin"></i>';
+          }
+          value = attributes['assessment_final']
+          if (value == '-')
+            return value;
+          return '' + parseInt(value) + '%'; 
+        }};
+      var modelsList = this.collection.models;
+      if (modelsList.length > 0)
+      {
+        for (groupworkIndex in modelsList[0].attributes.groupworks)
+        {
+          groupworkData = modelsList[0].attributes.groupworks[groupworkIndex];
+          groupwork = _.clone(assessment_template);
+          groupwork.title = 'Group Work: ' + groupworkData.label;
+          groupwork.name = 'groupworks.' + groupworkIndex + '.percent';
+          groupwork.actions = function(id, attributes) 
+          { 
+            var value = attributes.groupworks[groupworkIndex].percent
+            if (value == '.')
+            {
+              return '<i class="fa fa-spinner fa-spin"></i>';
+            }
+            if (value == '-')
+              return value;
+            return '' + parseInt(value) + '%'; 
+          };
+          coursesListDetailsViewGrid.colModel.push(groupwork); 
+        }
+        var assessmentData;
+        for (assessmentIndex in modelsList[0].attributes.assessments)
+        {
+          assessmentData = modelsList[0].attributes.assessments[assessmentIndex];
+          assessment = _.clone(assessment_template);
+          assessment.title = 'Assessment: ' + assessmentData.label;
+          assessment.name = 'assessments.' + assessmentIndex + '.percent';
+          assessment.actions = function(id, attributes) 
+          { 
+            var value = attributes.assessments[assessmentIndex].percent
+            if (value == '.')
+            {
+              return '<i class="fa fa-spinner fa-spin"></i>';
+            }
+            if (value == '-')
+              return value;
+            return '' + parseInt(value) + '%'; 
+          };
+          coursesListDetailsViewGrid.colModel.push(assessment); 
+        }
+      }
+      coursesListDetailsViewGrid.render();
+    },
   });
