0782214a440e1105538082dbb26a1a4c37346aec
Frano Sincic <fsincic@extensionengine.com>
2016-03-23 16:25:29 +0000

    fixed clone header

--- static/js/common/common_js.js
+++ static/js/common/common_js.js
@@ -58,3 +58,68 @@ highlightSearchBar = function(item) {
       val.css("background-color", "#B1C2CC");
   });
 }
+
+cloneHeader = function() {
+  console.log("usa");
+  var clonedHeader = $('#coursesListViewGridBlock').find('.bbGrid-grid-head').clone(true);
+  // $('#coursesListViewGridBlock').find('.bbGrid-grid-head').css("display", "none");
+  // console.log(clonedHeader);
+  
+  clonedHeader.css("position", "absolute");
+  clonedHeader.css("top", "0px");
+  clonedHeader.css("left", "0px");
+
+  clonedHeader.css("font-size", "12px");
+  clonedHeader.css("cursor", "pointer");
+  clonedHeader.css("background", "whitesmoke");
+  clonedHeader.css("border-collapse", "separate");
+  clonedHeader.css("border-spacing", "0");
+  clonedHeader.css("table-layout", "fixed");
+  var head = $('#coursesListViewGridBlock').find('.bbGrid-grid-head')[0];
+  var width = window.getComputedStyle(head).width;
+  var height = $('#coursesListViewGridBlock').find('.bbGrid-grid-head').height();
+  clonedHeader.css("width", width);
+  clonedHeader.css("height", height);
+
+  var tr = $('#coursesListViewGridBlock').find('.bbGrid-grid-head').find('tr')[0];
+  var trwidth = window.getComputedStyle(tr).width;
+  var trheight = $('#coursesListViewGridBlock').find('.bbGrid-grid-head').find('tr').height();
+  clonedHeader.find('tr').css("width", trwidth);
+  clonedHeader.find('tr').css("height", trheight);
+  clonedHeader.find('tr').css("border-radius", "4px 0 0 0");
+  
+  var thWidths = []
+  $('#coursesListViewGridBlock').find('.bbGrid-grid-head').find('th').each(function(index, value){
+    console.log($(value));
+    var width = window.getComputedStyle(value).width;
+    thWidths.push(width);
+  });
+  console.log(thWidths);
+  var i = 0
+  clonedHeader.find('th').each(function(index, value){
+    $(value).css("width", thWidths[i]);
+    i = i+1;
+  });
+
+  // var thwidth = $('#coursesListViewGridBlock').find('.bbGrid-grid-head').find('th').width();
+  var thheight = $('#coursesListViewGridBlock').find('.bbGrid-grid-head').find('th').height();
+  // clonedHeader.find('th').css("width", thwidth);
+  clonedHeader.find('th').css("height", thheight);
+  clonedHeader.find('th').css("background-color", "#f8f8f8");
+  clonedHeader.find('th').css("box-sizing", "content-box");
+  clonedHeader.find('th').css("overflow", "hidden");
+  clonedHeader.find('th').css("white-space", "normal");
+  clonedHeader.find('th').css("border-bottom", "1px solid #ccc");
+  clonedHeader.find('th').css("border-top", "1px solid #ccc");
+  clonedHeader.find('th').css("border-left", "1px solid #ccc");
+  clonedHeader.find('th').css("display", "table-cell");
+  clonedHeader.find('th').css("line-height", "1.28571rem");
+  clonedHeader.find('th').css("padding", ".57143rem .71429rem .71429rem");
+  clonedHeader.find('th').css("font-size", "1rem");
+  clonedHeader.find('th').css("font-weight", "bold");
+  clonedHeader.find('th').css("color", "#222");
+  clonedHeader.find('th').css("text-align", "left");
+  clonedHeader.find('th').css("margin", "0");
+  console.log(clonedHeader);
+  $('#coursesListViewGridBlock').prepend(clonedHeader);
+}
