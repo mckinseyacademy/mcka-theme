f11d60bc3cd82f0c58a99c128e8d27e7030e1d0f
Josip Kotarac <jkotarac@extensionengine.com>
2016-03-31 15:57:18 +0000

    fixed removing administrator rights when changeing permissions

--- admin/permissions.py
+++ admin/permissions.py
@@ -44,7 +44,11 @@ class Permissions(object):
     def remove_all_course_roles(self, course_id):
         per_course_roles = [{"course_id": p.course_id, "role": p.role}
                             for p in self.user_roles if p.course_id != course_id]
-        self.save([], per_course_roles)
+        new_perms = [
+                    perm for perm in self.current_permissions
+                    if perm not in (PERMISSION_GROUPS.MCKA_TA, PERMISSION_GROUPS.MCKA_OBSERVER)
+                ]
+        self.save(new_perms, per_course_roles)
 
     def update_course_role(self, course_id, role):
         per_course_roles = [{"course_id": p.course_id, "role": p.role}
@@ -53,7 +57,11 @@ class Permissions(object):
             "course_id": course_id,
             "role": role,
         })
-        self.save([], per_course_roles)
+        new_perms = [
+                    perm for perm in self.current_permissions
+                    if perm not in (PERMISSION_GROUPS.MCKA_TA, PERMISSION_GROUPS.MCKA_OBSERVER)
+                ]
+        self.save(new_perms, per_course_roles)
 
     def save(self, new_permissions, per_course_roles):
         try:
