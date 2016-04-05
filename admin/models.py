4c44e9ef3b103d9d9480aa77292f32dc52e5d2a4
Danko <danko@extensionengine.com>
2016-04-05 08:04:47 +0000

    MCKIN-4010 Discover content url form validation enabled

--- admin/models.py
+++ admin/models.py
@@ -437,7 +437,7 @@ class LearnerDashboardTile(db_models.Model):
 
 class LearnerDashboardDiscovery(db_models.Model):
 
-    link = db_models.CharField(max_length=80)
+    link = db_models.URLField()
     title = db_models.CharField(max_length=20)
     author = db_models.CharField(blank=True, max_length=20)
     position = db_models.IntegerField(default=1)
