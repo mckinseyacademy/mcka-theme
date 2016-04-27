2a1f23e49656555ff2e995ebe2b56a018dbd13d2
Danko <danko@extensionengine.com>
2016-04-27 12:45:20 +0000

    Removed ununnecessary import

--- accounts/middleware/learner_dashboard_check.py
+++ accounts/middleware/learner_dashboard_check.py
@@ -1,7 +1,6 @@
 ''' check Learner Dashboard existence for user/course/client '''
 from django.conf import settings
 from courses.models import FeatureFlags
-from courses.user_courses import get_current_course_for_user
 from api_client import user_api
 from accounts.controller import set_learner_dashboard_in_session
 
