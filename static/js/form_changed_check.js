6eeab5d78a36705a38ef9d3ea1690022c455a091
Kruno Munitic <kruno@extensionengine.com>
2016-04-28 12:01:31 +0000

    Form input select fix

--- static/js/form_changed_check.js
+++ static/js/form_changed_check.js
@@ -2,9 +2,6 @@ $(document).ready(function() {
   $('form input').on( 'input change', function() {
     $('#formSubmit').attr('disabled',false);
   });
-  $('form select').on( 'change', function() {
-    $('#formSubmit').attr('disabled',false);
-  }); 
 });
   
 
