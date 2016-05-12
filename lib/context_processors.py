4f2e27ae6085fc832c76d35f9b648d46d1abf4a4
Kruno Munitic <kruno@extensionengine.com>
2016-05-12 11:59:03 +0000

    MCKIN-4116 - for testing on qa

--- lib/context_processors.py
+++ lib/context_processors.py
@@ -72,3 +72,16 @@ def settings_data(request):
 
     return data
 
+def google_analytics(request):
+    """
+    Use the variables returned in this function to
+    render your Google Analytics tracking code template.
+    """
+    ga_prop_id = getattr(settings, 'GOOGLE_ANALYTICS_PROPERTY_ID', False)
+    ga_domain = getattr(settings, 'GOOGLE_ANALYTICS_DOMAIN', False)
+    if not settings.DEBUG and ga_prop_id and ga_domain:
+        return {
+            'GOOGLE_ANALYTICS_PROPERTY_ID': 'UA-77644470-1',
+            'GOOGLE_ANALYTICS_DOMAIN': '54.221.197.82',
+        }
+    return {}
