2c188a3d6ed98b80f47dbd1c187309d726125fc9
Dino Cikatic <dcikatic@edx.org>
2016-05-13 17:20:01 +0200

    Re-enable Progress Page V2

--- static/js/views/courses/progress.js
+++ static/js/views/courses/progress.js
@@ -1,14 +1,7 @@
 Apros.views.CourseProgress = Backbone.View.extend({
   initialize: function() {
-    var total = _(historicalBarChart[0].values).findWhere({label: 'TOTAL'});
-    delete total.label;
-    this.chart = new Apros.models.Chart({chart_data: historicalBarChart});
-    var el = this.$('#chart-grades').append(
-      $('<div />', {'class': 'course-total', html: 'TOTAL<div>' + total.value + '%</div>'})
-    );
   },
 
   render: function() {
-    this.chart.historical_bar_chart('#chart-grades svg');
   }
 });
