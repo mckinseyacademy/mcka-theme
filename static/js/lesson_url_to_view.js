7dbe750388a759f3a72809acd919bc68a0b6007a
Danko <danko@extensionengine.com>
2016-05-13 10:28:15 +0000

    Refactored javascripts

---
+++ static/js/lesson_url_to_view.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  var headers = {
+    'X-CSRFToken': $.cookie('apros_csrftoken')
+  }
+  $.ajax({
+    headers: headers,
+    dataType: 'json',
+    data: {
+      lesson_link: $('#course-lessons').data('course-url'),
+      tile_id: $('#course-lessons').data('tile-id'),
+    },
+    type: 'POST',
+    url: '/learnerdashboard/bookmark_lesson'
+  });
+});
