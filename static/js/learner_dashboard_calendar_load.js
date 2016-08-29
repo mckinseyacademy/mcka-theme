757cbe369f13cad5b50a4e36d0c2da7522abe745
Kruno Munitic <kruno@extensionengine.com>
2016-08-29 02:13:54 +0000

    Ajax js calendar loader

---
+++ static/js/learner_dashboard_calendar_load.js
@@ -0,0 +1,17 @@
+$('#open-calendar').click(function() {
+
+  var headers = {
+    'X-CSRFToken': $.cookie('apros_csrftoken')
+  }
+  $.ajax({
+    headers: headers,
+    type: 'POST',
+    url: '/learnerdashboard/calendar',
+    success : function(data) {
+      $('#replace-calendar').html(data.html);
+    },
+    error: function(xhr, status, error) {
+      var err = eval("(" + xhr.responseText + ")");
+    }
+  });
+});
