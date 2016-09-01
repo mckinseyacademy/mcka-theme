ce6f53530f07b3dfdb4905bd1698a9def325f09e
Kruno Munitic <kruno@extensionengine.com>
2016-09-01 15:59:37 +0000

    Calendar ui updates

--- static/js/learner_dashboard_calendar_load.js
+++ static/js/learner_dashboard_calendar_load.js
@@ -1,18 +1,24 @@
-$('#open-calendar').click(function() {
+var opened = false;
 
-  var headers = {
-    'X-CSRFToken': $.cookie('apros_csrftoken')
-  }
-  $.ajax({
-    headers: headers,
-    type: 'GET',
-    url: '/learnerdashboard/calendar',
-    success : function(data) {
-      $('#replace-calendar').html(data.html);
-    },
-    error: function(xhr, status, error) {
-      var err = eval("(" + xhr.responseText + ")");
-      console.log (err);
+$('#open-calendar').click(function() {
+  if (!opened) {
+
+    var headers = {
+      'X-CSRFToken': $.cookie('apros_csrftoken')
     }
-  });
+
+    $.ajax({
+      headers: headers,
+      type: 'GET',
+      url: '/learnerdashboard/calendar',
+      success : function(data) {
+        opened = true;
+        $('#replace-calendar').html(data.html);
+      },
+      error: function(xhr, status, error) {
+        var err = eval("(" + xhr.responseText + ")");
+        console.log (err);
+      }
+    });
+  }
 });
