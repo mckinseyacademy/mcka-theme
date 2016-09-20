81ce1ba61521aa509968ca34243b99e3fe4dc67f
Kruno Munitic <kruno@extensionengine.com>
2016-09-20 15:34:21 +0000

    Company LD added full disc, ele sort func

---
+++ static/js/admin_learner_dashboard/sort.js
@@ -0,0 +1,74 @@
+$(function() {
+  var tbody = $('tbody');
+  var isDragging;
+  var wasDragging;
+
+  tbody.sortable({
+    cursorAt: {
+      top: 0,
+      left: 0
+    },
+    axis: 'y'
+  });
+
+  $("#saveSortDiscover").click("sortupdate", function(event, ui) {
+    $.ajax({
+      dataType: 'json',
+      headers: {
+        'X-CSRFToken': $.cookie('apros_csrftoken')
+      },
+      data: {
+        position: $("#discover-body").sortable('toArray')
+      },
+      type: 'POST',
+      url: window.location.pathname + "/discover_reorder"
+    });
+
+    $("#saveSortDiscover").css("display", "none");
+    $("#savedSortDiscover").css("display", "inline");
+    setTimeout(function() {
+      $("#savedSortDiscover").fadeOut("slow");
+    }, 1000)
+  });
+
+  $("#saveSortElement").click("sortupdate", function(event, ui) {
+    $.ajax({
+      dataType: 'json',
+      headers: {
+        'X-CSRFToken': $.cookie('apros_csrftoken')
+      },
+      data: {
+        position: $("#element-body").sortable('toArray')
+      },
+      type: 'POST',
+      url: window.location.pathname + "/element_reorder"
+    });
+
+    $("#saveSortElement").css("display", "none");
+    $("#savedSortElement").css("display", "inline");
+    setTimeout(function() {
+      $("#savedSortElement").fadeOut("slow");
+    }, 1000)
+  });
+
+  tbody.disableSelection();
+
+  tbody.mousedown(function() {
+    isDragging = false;
+  }).mousemove(function() {
+    isDragging = true;
+  }).mouseup(function() {
+    wasDragging = isDragging;
+    isDragging = false;
+    if (wasDragging) {
+      $("#saveSortElement").css("display", "inline");
+      $("#saveSortDiscover").css("display", "inline");
+      $("#savedSortElement").css("display", "none");
+      $("#savedSortDiscover").css("display", "none");
+    }
+  });
+
+  $('.confirm').click(function() {
+    return window.confirm("Are you sure?");
+  });
+});
