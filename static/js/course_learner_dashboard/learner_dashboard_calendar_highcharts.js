0c30fefdbf8dc8fce247c7014176a4ad15d9a3cd
Kruno Munitic <kruno@extensionengine.com>
2016-10-24 13:45:05 +0000

    MCKIN-4584 tooltip not showing

--- static/js/course_learner_dashboard/learner_dashboard_calendar_highcharts.js
+++ static/js/course_learner_dashboard/learner_dashboard_calendar_highcharts.js
@@ -20,7 +20,7 @@ $(function() {
         xAxis: {
             categories: [''],
             min: 0,
-            max: (maxRow - 1),
+            max: (maxRow + 2),
             labels: {
                 enabled: false
             },
