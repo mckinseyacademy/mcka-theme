77c2b794378cf0bab42b030f93d0356a9f6ee05a
Naeem Ilyas <naeem-ilyas@live.com>
2017-03-27 13:40:55 +0000

    mckin-4861 fix and escape refactoring

--- static/js/utils.js
+++ static/js/utils.js
@@ -2,13 +2,32 @@
  * Generic util functions
  */
 
-Apros.utils.cleanParticipantProps = function (modelInstance, dataObj) {
-    // run escape on potentially malicious properties
+Apros.utils.cleanParticipantProps = function (dataObj) {
+    // run clean functions on potentially malicious properties
+
     _.each(dataObj, function(value, prop){
         if(_.contains(Apros.config.PARTICIPANT_PROPERTIES_TO_CLEAN, prop)){
-            dataObj[prop] = modelInstance.escape(prop);
+            value = _.escape(value);
+
+            if(value && Apros.config.FORMULA_CHARACTERS_ESCAPING){
+                value = Apros.utils.cleanFormulaInjectionCharacters(value);
+            }
+
+            dataObj[prop] = value;
         }
     });
 
-    return dataObj;        
+    return dataObj;
+};
+
+
+Apros.utils.cleanFormulaInjectionCharacters = function (string) {
+    // clean formula injection characters from string
+
+    var charString = Apros.config.CSV_CHARACTER_BLACKLIST.join('');
+    charString = '[' + charString + ']';
+
+    var regex = new RegExp(charString, 'gi');
+
+    return string.replace(regex, '');
 };
