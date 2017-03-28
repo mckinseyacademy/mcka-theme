38175955fa0c29f9f3baafe8e1d24f4712348419
Naeem Ilyas <naeem-ilyas@live.com>
2017-03-28 11:24:20 +0000

    code refactoring

--- static/js/utils.js
+++ static/js/utils.js
@@ -5,12 +5,14 @@
 Apros.utils.cleanParticipantProps = function (dataObj) {
     // run clean functions on potentially malicious properties
 
+    var formulaCleanRegex = Apros.utils.getFormulaCleanRegex();
+
     _.each(dataObj, function(value, prop){
         if(_.contains(Apros.config.PARTICIPANT_PROPERTIES_TO_CLEAN, prop)){
-            value = _.escape(value);
+            value = Apros.utils.escapeHtml(value);
 
             if(value && Apros.config.FORMULA_CHARACTERS_ESCAPING){
-                value = Apros.utils.cleanFormulaInjectionCharacters(value);
+                value = Apros.utils.cleanFormulaInjectionCharacters(value, formulaCleanRegex);
             }
 
             dataObj[prop] = value;
@@ -21,13 +23,30 @@ Apros.utils.cleanParticipantProps = function (dataObj) {
 };
 
 
-Apros.utils.cleanFormulaInjectionCharacters = function (string) {
-    // clean formula injection characters from string
+Apros.utils.getFormulaCleanRegex = function () {
+    // construct formula clean regex
 
     var charString = Apros.config.CSV_CHARACTER_BLACKLIST.join('');
     charString = '[' + charString + ']';
 
-    var regex = new RegExp(charString, 'gi');
+    return new RegExp(charString, 'gi');
+};
+
+
+Apros.utils.escapeHtml = function (string) {
+    // escape HTML entities in string
+    // e.g; '<script>' is changed to '&lt;script&gt;'
+
+    return _.escape(string);
+};
+
+
+Apros.utils.cleanFormulaInjectionCharacters = function (string, regex) {
+    // clean formula injection characters from string using passed regex
+    // e.g; 'cmd|calc' is changed to 'cmd calc'
+
+    if(!regex)
+        regex = Apros.utils.getFormulaCleanRegex();
 
     return string.replace(regex, '');
 };
