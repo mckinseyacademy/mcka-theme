39c2f9fd8d2e6eec29ef791d53828c5c4a73e114
zvishal <vishalhgandhi@gmail.com>
2017-04-05 21:27:32 -0400

    Removed google from sign up validation form

--- accounts/forms.py
+++ accounts/forms.py
@@ -305,7 +305,7 @@ CURRENT_ROLE = (
 
 BANNED_EMAILS = [
   "aol.com", "att.net", "comcast.net", "facebook.com", "gmail.com", "gmx.com", "googlemail.com",
-  "google.com", "hotmail.com", "hotmail.co.uk", "mac.com", "me.com", "mail.com", "msn.com",
+  "hotmail.com", "hotmail.co.uk", "mac.com", "me.com", "mail.com", "msn.com",
   "live.com", "sbcglobal.net", "verizon.net", "yahoo.com", "yahoo.co.uk",
   "email.com", "games.com", "gmx.net", "hush.com", "hushmail.com", "icloud.com", "inbox.com",
   "lavabit.com", "love.com", "outlook.com", "pobox.com", "rocketmail.com",
