f4af25c221c98696c8a57f9ff891dcd8f806c01f
Saqib <msaqib52@gmail.com>
2017-11-10 20:12:29 +0500

    update appl-app-site-association json and revert content-type

--- mobile_app_associations/controller.py
+++ mobile_app_associations/controller.py
@@ -6,7 +6,7 @@ import os
 from django.http import HttpResponse, Http404
 
 
-def get_app_association_file_response(filename, content_type='application/json'):
+def get_app_association_file_response(filename):
     """
     Helper method to get app association json file response
     """
@@ -18,7 +18,7 @@ def get_app_association_file_response(filename, content_type='application/json')
     try:
         with open(asset_links_file_path) as asset_links_file:
             android_assets_link_json = asset_links_file.read()
-            response = HttpResponse(android_assets_link_json, content_type=content_type)
+            response = HttpResponse(android_assets_link_json, content_type='application/json')
             response['Content-Disposition'] = 'attachment; filename={}'.format(filename)
             return response
     except IOError:
