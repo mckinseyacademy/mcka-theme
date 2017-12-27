eb701e0ead0338cba3ecf90bdee9813371567ccf
murad <heyub1gm0uth>
2017-12-27 16:19:01 +0500

    Popup fix

--- static/js/app_link_popup.js
+++ static/js/app_link_popup.js
@@ -1,33 +1,42 @@
- $(function(){
-      if($('#mobileAppLinkModal').length > 0){
+$(function(){
+    if($('#mobileAppLinkModal').length > 0){
         $('#mobileAppLinkModal').foundation('reveal', 'open');
-      }
+    }
 
-      function trackAppDownloadLinkClick(link, username, platform) {
+    function trackAppDownloadLinkClick(link, username, platform) {
         setTimeout(visitLink, 1000);
 
         var link_visited = false;
 
         function visitLink() {
-          if (!link_visited) {
-            link_visited = true;
-            window.location.href = link;
-          }
+            if (!link_visited) {
+                link_visited = true;
+                window.location.href = link;
+            }
         }
 
         ga('send', 'event', 'Outbound Link', 'click', platform + ' App Download', {
-          'username': username,
-          hitCallback: visitLink
+            'username': username,
+            hitCallback: visitLink
         });
 
-      }
-      $("#ios_download_link").on('click', function(event) {
+    }
+    $("#ios_download_link").on('click', function(event) {
         event.preventDefault();
         trackAppDownloadLinkClick(this.href, $(this).data('username'), 'iOS' )
-      });
-      $("#android_download_link").on('click', function(event) {
+    });
+    $("#android_download_link").on('click', function(event) {
         event.preventDefault();
         trackAppDownloadLinkClick(this.href, $(this).data('username'), 'Android' )
-      });
+    });
 
-      });
+});
+
+// Diable scrollbar on popup show
+
+$(document).on('open', '#mobileAppLinkModal', function () {
+    $('body').css("overflow", "hidden");
+});
+$(document).on('close', '#mobileAppLinkModal', function () {
+    $('body').css("overflow", "auto");
+});
