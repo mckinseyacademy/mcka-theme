828df6e98dddceee78e53f16018fb08d37f00497
Tahir Fazal <tahir.fazal@arbisoft.com>
2018-01-17 15:57:31 +0500

    Replace hardcoded strings in haml templates  6653 (#1836)
    * setup i18n coverage testing
    
    * [#6653] Adding trans in certificates (#1835)
    
    * setup i18n and internationalize login page
    
    * Upgrade django rest framework from v2.3.14 to v3.2.3 (#1816)
    
    MCKIN-6532 upgrade djangorestframework to version 3.2.3
    
    * MCKIN-6541 remove ruby and update docs and provisioning sript accordingly
    
    * [#6653] Adding trans in certificates
    
    * [#6653] Updating some trans strings
    
    * [#6653] Update certificates translations
    
    * [#6653] Update translations in certificates
    
    * Replace hardcoded strings registrations 6653 (#1845)
    
    * setup i18n coverage testing
    
    * [#6653] Adding trans in registration application
    
    * [#6653] Button value translation
    
    * [#6653] Update translations in REGISTRATIONS
    
    * Replace hardcoded Accounts strings mckin 6653 (#1828)
    
    * setup i18n coverage testing
    
    * [#6653] Updating haml templates for accounts application.
    
    * [#6653] Update accounts translations.
    
    * [#6653] Update accounts translations.
    
    * [#6653] Update accounts translations.
    
    * [#6653] Update translations in accounts
    
    * [#6653] Update translations in accounts
    
    * [#6653] Update translations in COURSES
    
    * [#6653] Replace hard coded strings courses (#1837)
    
    * setup i18n coverage testing
    
    * Adding trans in courses
    
    * [#6653] Update courses translations
    
    * [#6653] Update translations in COURSES
    
    * [#6653] Update translations in COURSES
    
    * [#6653] Update translations in COURSES
    
    * [#6653] Update translations in COURSES
    
    * [#6653] Update translations in COURSES
    
    * [#6653] Update courses translations
    
    * [#6653] Update courses translations tags.
    
    * [#6653] Update courses translations tags.
    
    * Replace hardcoded strings admin 6653 (#1848)
    
    * setup i18n coverage testing
    
    * [#6653] Adding translations in admin application
    
    * [#6653] Update messages
    
    * [#6653] Update messages
    
    * [#6653] Update translations
    
    * [#6653] Update translations
    
    * [#6653] Update admin translations
    
    * [#6653] Update admin translations
    
    * [#6653] Update admin translations
    
    * [#6653] Update admin translations
    
    * [#6653] Update admin translations
    
    * [#6653] Update admin translations
    
    * [#6653] Update admin translations tags.
    
    * [#6653] Remove translation from email templates
    
    * [#6653] Updating Po files

--- mcka_apros/settings.py
+++ mcka_apros/settings.py
@@ -79,6 +79,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
