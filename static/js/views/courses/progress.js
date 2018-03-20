544176c2b6bee052f81b997c72f9c3b8e076230a
Murad Habib <murad.habib@arbisoft.com>
2018-03-20 15:51:19 +0500

    Progress and discover (#1971)

--- static/js/views/courses/progress.js
+++ static/js/views/courses/progress.js
@@ -4,4 +4,14 @@ Apros.views.CourseProgress = Backbone.View.extend({
 
   render: function() {
   }
+
+});
+
+// Progress Total and Cohort Average indicator for RTL
+
+$(document).ready(function(){
+  var progressTotal = $(".rtl .course-progress .visualization .total");
+  var cohortPointer = $(".rtl .course-progress .visualization .triangle");
+  $(progressTotal).css("right", progressTotal.css('left'));
+  $(cohortPointer).css("right", cohortPointer.css('left'));
 });
