032835e4e25f68a2c7e273507a111aba63adf571
Zaman Afzal <zamanafzal@gmail.com>
2018-04-26 13:07:29 +0500

    Mckin7321 Add first name and last name in an attribute to show in grid if anyone of them is available. (#2021)
    * add first name and last name in an attribute to show in grid and enable search on it 7321
    
    * change the check from and operator to or operator for showing custom full name 7321

--- static/js/models/course_details.js
+++ static/js/models/course_details.js
@@ -1,7 +1,7 @@
   Apros.models.Course_Details = Backbone.Model.extend({
     parse: function (data, options) {
         data.custom_full_name = data.full_name;
-        if(data.first_name && data.last_name)
+        if(data.first_name || data.last_name)
             data.custom_full_name = data.first_name+ " "+ data.last_name;
         return Apros.utils.cleanProps(
             data, 
