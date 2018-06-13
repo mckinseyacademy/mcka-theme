683b613cfc92ef4782cbc8404f341fb48d7e2354
Wajeeha Khalid <wajeeha.khalid@arbisoft.com>
2018-06-13 15:47:25 +0500

    MCKIN-7739: updated management command to use CSV provided by ops team (#2103)
    * MCKIN-7739: updated management command to use CSV provided by ops team

--- main/management/commands/unenroll_students.py
+++ main/management/commands/unenroll_students.py
@@ -1,33 +1,41 @@
 import json
 import os
+import csv
 
 from django.core.management.base import BaseCommand
 from django.conf import settings
 
 from api_client import user_api
 from api_client.api_error import ApiError
+from mcka_apros.settings import BASE_DIR
+
 
 class Command(BaseCommand):
     help = 'Unenrolls students from courses'
 
     def handle(self, *args, **options):
-        unenroll_msg = "Unenrolling students..."
-        self.stdout.write(unenroll_msg)
-        if len(args) == 0:
-            raise CommandError("A file path must be specified")
-        filename = args[0]
-        if not os.path.isfile(filename):
-            raise CommandError("File does not exist at the specified path.")
+        self.stdout.write("Unenrolling Users STARTED")
 
-        with open(filename) as json_file:
-            course_list = json.load(json_file)
-            for course in course_list:
+        file_path = os.path.join(BASE_DIR, 'main/fixtures/un-enroll_users.csv')
 
-                for uid in course["users"]:
-                    try:
-                        print "Unenrolling uid: %d from course: %s" % (uid, course["id"])
-                        user_api.unenroll_user_from_course(uid, course["id"])
-                    except ApiError as e:
-                        print "User: %d not unenrolled" % (uid)
+        with open(file_path) as csvfile:
+            reader = csv.DictReader(csvfile)
+            successful_unenrolled_count = 0
+            unsuccessful_unenrolled_count = 0
+            for row in reader:
+                user_id = int(row['Participant_ID'])
+                course_id = row['Course_ID']
+                try:
+                    print "Unenrolling uid: %d from course: %s" % (user_id, course_id)
+                    user_api.unenroll_user_from_course(user_id, course_id)
+                    successful_unenrolled_count += 1
+                except ApiError as e:
+                    unsuccessful_unenrolled_count +=1
+                    print "User: %d not unenrolled" % (user_id)
 
-        self.stdout.write("Done")
+        self.stdout.write(
+            "Unenrolling Users COMPLETED with {0} out of {1} users successfully un-enrolled.".format(
+                successful_unenrolled_count,
+                successful_unenrolled_count + unsuccessful_unenrolled_count
+            )
+        )
