2c75f52d2d4919cf23d88bd2b9ef51dcaba49cc5
Kuldeep Kumar <langhanikuldeep@gmail.com>
2018-07-09 18:46:15 +0500

    MCKIN-7906-add-bash-file (#2129)

---
+++ docs/translations.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# This file is for devstack and developers only for running traslation commands.
+# Usage:
+# 1: sudo chmod +x filename.sh
+# 2: ./translations.sh
+
+for lang in locale/*
+do
+	bn=$(basename $lang)
+	if [ $bn != 'config.yaml' ]
+	then
+		echo "translating locale $bn"
+		django-admin makemessages -l $bn --extension=html,haml,py
+	 	django-admin makemessages --locale=$bn --domain=djangojs --ignore=gen/* --ignore=static_cache/* --ignore=vendor/*
+		python manage.py compilemessages -l $bn
+	fi
+done
