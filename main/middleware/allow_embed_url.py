42431bf56697cef47d3e51e34dac8be84c38f7b7
Naeem Ilyas <naeem-ilyas@live.com>
2018-08-10 11:23:48 +0000

    fix allow embed urls middleware

--- main/middleware/allow_embed_url.py
+++ main/middleware/allow_embed_url.py
@@ -7,23 +7,39 @@ class AllowEmbedUrlMiddleware(object):
     is_scorm_shell = False
 
     def process_request(self, request):
-        # build list of allowed hosts from allowed URLs, remove *'s for easy matching
+        self.is_scorm_shell = False
+
+        if 'HTTP_REFERER' in request.META:
+            referrer_url = request.META['HTTP_REFERER']
+            self.is_scorm_shell = self.in_allowed_embed_urls(referrer_url)
+
+    def process_response(self, request, response):
+        referrer_url = request.COOKIES.get('referrer_url')
+
+        if referrer_url:
+            self.is_scorm_shell = self.in_allowed_embed_urls(referrer_url)
+
+        if self.is_scorm_shell:
+            response['Content-Security-Policy'] = 'frame-ancestors ' + settings.ALLOW_EMBED_URL
+            response['X-Frame-Options'] = 'ALLOW-FROM ' + settings.ALLOW_EMBED_URL
+
+        response.delete_cookie('referrer_url')
+
+        return response
+
+    def in_allowed_embed_urls(self, referrer_url):
+        """
+        checks if a domain is allowed to embed Apros in an iframe
+        """
+        # build list of allowed hosts from allowed URLs,
+        # remove *'s for easy matching
         allowed_hosts = map(
             lambda url: urlparse(url).hostname.replace('*', ''),
             settings.ALLOW_EMBED_URL.split()
         )
 
-        if 'HTTP_REFERER' in request.META:
-            referer_url = request.META['HTTP_REFERER']
+        for allowed_host in allowed_hosts:
+            if referrer_url.find(allowed_host) != -1:
+                return True
 
-            for allowed_host in allowed_hosts:
-                if referer_url.find(allowed_host) != -1:
-                    self.is_scorm_shell = True
-                    break
-
-    def process_response(self, request, response):
-        if self.is_scorm_shell:
-            response['Content-Security-Policy'] = 'frame-ancestors ' + settings.ALLOW_EMBED_URL
-            response['X-Frame-Options'] = 'ALLOW-FROM ' + settings.ALLOW_EMBED_URL
-
-        return response
+        return False
--- main/middleware/test.py
+++ main/middleware/test.py
@@ -1,12 +1,16 @@
+import ddt
+
 from django.test import TestCase, override_settings, RequestFactory, Client
-from accounts.views import home
 from django.core.urlresolvers import reverse
+
 from main.middleware.allow_embed_url import AllowEmbedUrlMiddleware
 
-ALLOW_EMBED_URL = 'https://example.com'
 
+ALLOW_EMBED_URL = 'https://example.com https://example2.com'
+
+
+@ddt.ddt
 class AllowEmbedUrlTest(TestCase):
-
     @override_settings(ALLOW_EMBED_URL=ALLOW_EMBED_URL)
     def test_details(self):
 
@@ -22,8 +26,52 @@ class AllowEmbedUrlTest(TestCase):
         client = Client()
         response = client.get(reverse('home'))
 
-        middleware.process_response(request,response)
+        middleware.process_response(request, response)
 
         self.assertEqual(middleware.is_scorm_shell, True)
         self.assertEqual(response['Content-Security-Policy'], 'frame-ancestors ' + ALLOW_EMBED_URL)
         self.assertEqual(response['X-Frame-Options'], 'ALLOW-FROM ' + ALLOW_EMBED_URL)
+
+    @override_settings(ALLOW_EMBED_URL=ALLOW_EMBED_URL)
+    @ddt.data(
+        ('https://example.com/a/url/', True),
+        ('https://abc.com/a/url', False),
+        ('https://example2.com/a/url', True),
+    )
+    @ddt.unpack
+    def test_by_referrer_cookie(self, referrer_url, is_scorm_shell):
+        """
+        tests middleware by referrer cookie
+        """
+        middleware = AllowEmbedUrlMiddleware()
+
+        request = RequestFactory().get('/')
+        request.COOKIES['referrer_url'] = referrer_url
+
+        middleware.process_request(request)
+
+        client = Client()
+        response = client.get(reverse('home'))
+
+        middleware.process_response(request, response)
+
+        self.assertEqual(middleware.is_scorm_shell, is_scorm_shell)
+
+        if is_scorm_shell:
+            self.assertEqual(response['Content-Security-Policy'], 'frame-ancestors ' + ALLOW_EMBED_URL)
+            self.assertEqual(response['X-Frame-Options'], 'ALLOW-FROM ' + ALLOW_EMBED_URL)
+
+    @override_settings(ALLOW_EMBED_URL=ALLOW_EMBED_URL)
+    @ddt.data(
+        ('https://example.com/a/url/', True),
+        ('https://abc.com/a/url', False),
+        ('https://example2.com/a/url', True),
+    )
+    @ddt.unpack
+    def test_in_allowed_embed_urls(self, referrer_url, expected):
+        """
+        test helper method for checking allowed urls
+        """
+        middleware = AllowEmbedUrlMiddleware()
+
+        self.assertEqual(middleware.in_allowed_embed_urls(referrer_url), expected)
