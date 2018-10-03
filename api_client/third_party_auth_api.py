6fb98c81d71e9f44b20550ff8833cccf94c42ab3
Kshitij Sobti <kshitij@sobti.in>
2018-10-03 17:22:39 +0530

    Simplify get_providers_by_email and add support for fetching by username (#2209)

--- api_client/third_party_auth_api.py
+++ api_client/third_party_auth_api.py
@@ -1,39 +1,28 @@
-from urllib import urlencode
+from urllib2 import HTTPError
+
 from django.conf import settings
+
 from api_client import user_models
+from api_client.api_error import api_error_protect
 from .json_object import JsonParser as JP
 from .json_requests import GET
-
-from api_client.api_error import api_error_protect
 
 USER_API = getattr(settings, 'USER_API', 'api/server/users')
 THIRD_PARTY_AUTH_API = getattr(settings, 'THIRD_PARTY_AUTH_API', 'api/third_party_auth/v0')
 
 
 @api_error_protect
-def get_providers_by_email(email):
-    qs_params = {'email': email}
-    response = GET('{server_addr}/{api}/?{qs_params}'.format(
-        server_addr=settings.API_SERVER_ADDRESS,
-        api=USER_API,
-        qs_params=urlencode(qs_params)
-    ))
-    users = JP.from_json(response.read(), user_models.UserListResponse).results
-
-    if not users:
-        return []
-
-    # should never have two users with same email - if that happened probably there're some problem with DB and auth
-    assert len(users) <= 1
-
-    user = users[0]
-
-    response = GET("{server_addr}/{api}/users/{username}".format(
-        server_addr=settings.API_SERVER_ADDRESS,
-        api=THIRD_PARTY_AUTH_API,
-        username=user.username
-    ))
+def get_providers_by_login_id(login_id):
+    try:
+        response = GET('{server_addr}/{api}/users/{login_id}'.format(
+            server_addr=settings.API_SERVER_ADDRESS,
+            api=THIRD_PARTY_AUTH_API,
+            login_id=login_id,
+        ))
+    except HTTPError as e:
+        if e.code == 404:
+            return []
+        else:
+            raise
     associations = JP.from_json(response.read(), user_models.UserSSOProviderAssociationList)
     return associations.active
-
-
