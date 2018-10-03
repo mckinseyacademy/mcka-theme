047ba6f8588e36dd4b1bfb576bb20018e3985c8d
Umar Asghar <mrumar.mughal824@outlook.com>
2018-10-03 09:44:22 -0400

    [MCKIN-8391] Calculate team average progress and proficiency (#2215)
    * calculate team average progress and proficiency
    
    * show 0 value of average
    
    * MCKIN-8514 Manager progress and proficiency cards UI
    
    * add i18n for text and fix formula
    
    * fix design
    
    * fix merge conflicts
    
    * remove commented code
    
    * fix merge conflict

--- static/js/views/admin/manager_dashboard.js
+++ static/js/views/admin/manager_dashboard.js
@@ -58,10 +58,28 @@ Apros.views.ManagerDashboardView = Backbone.View.extend({
       colModel: this.generatedGridColumns,
       onReady: function () {
 
+        var totalProgress = 0;
+        var totalProficiency = 0;
+        var total = 0;
         // Progressbar for progress column
         $('#managerDashboardWrapper td.progress').each(function (index, value) {
-            $(this).css("width" , $(this).text());
+            var text = $(this).text();
+            $(this).css("width" , text);
+            totalProgress += parseInt(text.replace("%", ""));
+            total++;
         });
+
+        $('#managerDashboardWrapper td.proficiency').each(function (index, value) {
+          var text = $(this).text();
+          $(this).css("width" , text);
+            totalProficiency += parseInt(text.replace("%", ""));
+        });
+
+        // Set the Average progess and proficiency
+        if(total) {
+            $('.progress-average large').text(parseInt(totalProgress / total) + "%");
+            $('.proficiency-average large span').text(parseInt(totalProficiency / total));
+        }
 
         // Activation status icon
         $('#managerDashboardWrapper .status').each(function () {
@@ -71,6 +89,7 @@ Apros.views.ManagerDashboardView = Backbone.View.extend({
         else{
             $(this).addClass("no")
         }});
+
       }
     });
     $('.bbGrid-container').append('<i class="fa fa-spinner fa-spin"></i>');
