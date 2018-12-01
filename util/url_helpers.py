5fe1a6bd5122c0b1326129272c23930cf16f4448
Kuldeep Kumar <langhanikuldeep@gmail.com>
2018-12-01 12:07:22 +0500

    MCKIN-8856-Fix language- dropdown (#2404)

--- util/url_helpers.py
+++ util/url_helpers.py
@@ -3,4 +3,4 @@ from urlparse import urlparse
 
 def get_referer_from_request(request):
     referer = request.META.get('HTTP_REFERER')
-    return urlparse(referer).path or None
+    return urlparse(referer).path if referer else None
