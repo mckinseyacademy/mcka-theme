d6479a9a60f93fcf740616b4a52be0be024666fc
Zaman Afzal <zamanafzal@gmail.com>
2018-12-31 17:12:53 +0500

    MCKIN-7520 Notify user about lockout after max number of wrong passwo… (#2461)
    * MCKIN-7520 Notify user about lockout after max number of wrong password attempts
    
    * MCKIN-7520 add user lock out tests and fix the invalid password test
    
    * MCKIN-7520 change function name

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -16,7 +16,7 @@ from mock import Mock, patch
 
 from accounts.controller import ProcessAccessKeyResult
 from accounts.models import RemoteUser
-from accounts.tests.utils import ApplyPatchMixin, make_user, AccessKeyTestBase
+from accounts.tests.utils import ApplyPatchMixin, make_user, AccessKeyTestBase, make_side_effect_raise_api_error
 from accounts.views import (MISSING_ACCESS_KEY_ERROR, MOBILE_URL_SCHEME_COOKIE, _build_mobile_redirect_response,
                             _cleanup_username as cleanup_username, access_key, finalize_sso_mobile, sso_error,
                             sso_finalize, sso_launch, switch_language_based_on_preference, get_user_from_login_id,
@@ -682,8 +682,24 @@ class LoginViewTest(TestCase, ApplyPatchMixin):
     def test_login_normal_invalid_password(self, login_id, mock_authenticate, mock_get_username):
         mock_get_username.return_value = DottableDict({"username": "test", "is_active": True})
         mock_authenticate.return_value = None
+        self.apply_patch('api_client.user_api.authenticate', make_side_effect_raise_api_error(401))
+        user_lockout_status = self.apply_patch('accounts.controller.get_user_lock_out_status')
+        user_lockout_status.return_value = False
         response = self.client.post(reverse('login'), {'login_id': login_id, 'password': 'password'})
         self.assertIn('{"password": "Password doesn\'t match our records. Try again."}', response.content)
+
+    @patch('accounts.views.get_user_from_login_id')
+    @patch('accounts.views.auth.authenticate')
+    @ddt.data('johndoe', 'john@doe.org')
+    def test_login_normal_user_lock_out(self, login_id, mock_authenticate,
+                                        mock_get_username):
+        mock_get_username.return_value = DottableDict({"username": "test", "is_active": True})
+        mock_authenticate.return_value = None
+        self.apply_patch('api_client.user_api.authenticate', make_side_effect_raise_api_error(403))
+        user_lockout_status = self.apply_patch('accounts.controller.get_user_lock_out_status')
+        user_lockout_status.return_value = True
+        response = self.client.post(reverse('login'), {'login_id': login_id, 'password': 'password'})
+        self.assertIn('{"lock_out": true}', response.content)
 
     @patch('accounts.views._process_authenticated_user')
     @patch('accounts.views.get_user_from_login_id')
