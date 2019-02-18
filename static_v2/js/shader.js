06c70eee8d488bc0e6a69b67fb92eb61fd7863b5
Nasir Hussain <nasirhjafri@gmail.com>
2019-02-18 20:00:40 +0500

    added pollyfix for var on IE (#2588)

--- static_v2/js/shader.js
+++ static_v2/js/shader.js
@@ -6,7 +6,7 @@
 function addLight(color, amount) {
     let cc = parseInt(color, 16) + amount;
     let c = (cc > 255) ? 255 : (cc);
-    c = (c.toString(16).length > 1) ? c.toString(16) : `0${c.toString(16)}`;
+    c = (c.toString(16).length > 1) ? c.toString(16) : "0" + c.toString(16);
     return c;
 }
 
@@ -14,14 +14,14 @@ function addLight(color, amount) {
 function lighten(color, amount) {
     color = (color.indexOf("#") >= 0) ? color.substring(1, color.length) : color;
     amount = parseInt((255 * amount) / 100);
-    return color = `#${addLight(color.substring(0, 2), amount)}${addLight(color.substring(2, 4), amount)}${addLight(color.substring(4, 6), amount)}`;
+    return color = "" + addLight(color.substring(0, 2), amount) + addLight(color.substring(2, 4), amount) + addLight(color.substring(4, 6), amount);
 }
 
 /* Resta el porcentaje indicado a un color (RR, GG o BB) hexadecimal para oscurecerlo */
 function subtractLight(color, amount) {
     let cc = parseInt(color, 16) - amount;
     let c = (cc < 0) ? 0 : (cc);
-    c = (c.toString(16).length > 1) ? c.toString(16) : `0${c.toString(16)}`;
+    c = (c.toString(16).length > 1) ? c.toString(16) : "0" + c.toString(16);
     return c;
 }
 
@@ -29,7 +29,7 @@ function subtractLight(color, amount) {
 function darken(color, amount) {
     color = (color.indexOf("#") >= 0) ? color.substring(1, color.length) : color;
     amount = parseInt((255 * amount) / 100);
-    return color = `#${subtractLight(color.substring(0, 2), amount)}${subtractLight(color.substring(2, 4), amount)}${subtractLight(color.substring(4, 6), amount)}`;
+    return color = "#" + subtractLight(color.substring(0, 2), amount) + subtractLight(color.substring(2, 4), amount) + subtractLight(color.substring(4, 6), amount);
 }
 
 
