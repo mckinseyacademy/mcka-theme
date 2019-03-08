ccbe4f3cbcf9dacbcfaee18a4c1287ef67871249
prakashpathak02 <46211967+prakashpathak02@users.noreply.github.com>
2019-03-08 18:05:41 +0530

    Prakash/xblock fixes (#51)
    * drang and drop - colors
    
    * icons updated for drag and drop
    
    * drag and drop, image exp, problem fixes
    
    * variables synced with  new UI
    
    * play icon in iphone fixed. Drag and drop if image, no background color
    
    * The background color and font changes to old UI styles when I click on the Discussions board.
    
    * heading style added in discussion tab
    
    * discussion heading + pop up
    
    * discussion page - updated radio and checkbox as per design
    
    * Feedback of MCKIN-9113
    
    * radio button applied on survey forms
    
    * popup improved as per feedback

--- lms/static/js/apros-xblocks.js
+++ lms/static/js/apros-xblocks.js
@@ -1,13 +1,14 @@
 $(function () {
+  var isSurveyTableFilled = false;
   $(document).on('change', '.new-theme input[type=radio]', function(e) {
     var parent = $(e.target).parents('[data-block-type="pb-mcq"]');
     $(parent).find(".choice-selector").removeClass("selected");
     $(e.target).parent().addClass("selected");
   });
 
-  $(document).on('change', '.new-theme [data-block-type="pb-mrq"] input[type="checkbox"]', function (e) {
-    if (e.target.checked) {
-      $(e.target).parent().addClass("selected");
+  $(document).on('change', '.new-theme [data-block-type="pb-mrq"] input[type="checkbox"], .new-theme .post-options input[type="checkbox"]', function (e) {
+    if (event.target.checked) {
+      $(event.target).parent().addClass("selected");
     } else {
       $(e.target).parent().removeClass("selected");
     }
@@ -19,8 +20,32 @@ $(function () {
     $(e.target).parent().addClass("selected");
   });
 
+  $(document).on('change', '.new-theme input[type=radio]', function (e) {
+    var parent = $(e.target).parents('.field-label');
+    $(parent).find(".selected").removeClass("selected");
+    $(event.target).parent().addClass("selected");
+  });
+
   // Add selected class to selected poll results when appended to DOM
-  $(document).on('DOMNodeInserted', '.poll-results-wrapper', function(){
-    $(".new-theme .poll-results input[type=radio]:checked").parent().addClass('selected');
-  })
+  $(document).on('DOMNodeInserted', '.poll-results-wrapper, .forum-new-post-form, .edit-post-form', function(){
+    $(".new-theme .poll-results input[type=radio]:checked, .new-theme input[type=radio]:checked").parent().addClass('selected');
+  });
+  $(document).on('DOMNodeInserted', '.forum-new-post-form', function(){
+    $(".new-theme input[type=checkbox]:checked").parent().addClass('selected');
+  });
+  $(document).on('DOMNodeInserted', '.lesson-content', function(){
+    if(!isSurveyTableFilled) {
+      isSurveyTableFilled = true;
+      setTimeout(function(){
+        surveyTableLabelPositionsForMobile();
+      }, 3000);
+    }
+  });
 });
+function surveyTableLabelPositionsForMobile(){
+  $('.survey-table .survey-option').each(function(index, element){
+    var span = $(element).find('.visible-mobile-only').prop('outerHTML');
+    $(element).find('.visible-mobile-only').remove();
+    $(element).append(span);
+  });
+}
