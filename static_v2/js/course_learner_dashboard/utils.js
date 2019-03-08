039054d92cdefb81b9b42eac1b659b2ea4545632
Hussnain Raza <hussnain.raza@arbisoft.com>
2019-03-08 17:25:50 +0500

    Mckin 9256 progress page fixing v2 (#2675)
    * MCKIN-9256
    - UI modifications.
    - Scroll Issue fix for the titles.

--- static_v2/js/course_learner_dashboard/utils.js
+++ static_v2/js/course_learner_dashboard/utils.js
@@ -1,5 +1,15 @@
 $(function(){
 
+   if ($(window).width() > 992)
+   {
+       if ($('#learnerDashboardGallery .col-md-4').length > 9)
+       {
+           $('#learnerDashboardGallery').removeClass('noScrollAppears');
+       }
+
+   }
+
+
    if ($(window).width() < 1025) {
            $('.gallery.mCustomScrollbar._mCS_1').removeClass('mCustomScrollbar _mCS_1');
 
