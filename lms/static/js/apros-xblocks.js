91ba459c76de36cbabea10b69a4141943656ea50
Prakash Pathak <prakash_pathak@external.mckinsey.com>
2019-03-08 11:12:54 +0530

    discussion page - updated radio and checkbox as per design

--- lms/static/js/apros-xblocks.js
+++ lms/static/js/apros-xblocks.js
@@ -5,7 +5,7 @@ $(function () {
     $(event.target).parent().addClass("selected");
   });
 
-  $(document).on('change', '.new-theme [data-block-type="pb-mrq"] input[type="checkbox"]', function (event) {
+  $(document).on('change', '.new-theme [data-block-type="pb-mrq"] input[type="checkbox"], .new-theme .post-options input[type="checkbox"]', function (event) {
     if (event.target.checked) {
       $(event.target).parent().addClass("selected");
     } else {
@@ -19,8 +19,17 @@ $(function () {
     $(event.target).parent().addClass("selected");
   });
 
+  $(document).on('change', '.new-theme input[type=radio]', function (e) {
+    var parent = $(e.target).parents('.field-label');
+    $(parent).find(".selected").removeClass("selected");
+    $(event.target).parent().addClass("selected");
+  });
+
   // Add selected class to selected poll results when appended to DOM
-  $(document).on('DOMNodeInserted', '.poll-results-wrapper', function(){
-    $(".new-theme .poll-results input[type=radio]:checked").parent().addClass('selected');
-  })
+  $(document).on('DOMNodeInserted', '.poll-results-wrapper, .forum-new-post-form, .edit-post-form', function(){
+    $(".new-theme .poll-results input[type=radio]:checked, .new-theme input[type=radio]:checked").parent().addClass('selected');
+  });
+  $(document).on('DOMNodeInserted', '.forum-new-post-form', function(){
+    $(".new-theme input[type=checkbox]:checked").parent().addClass('selected');
+  });
 });
