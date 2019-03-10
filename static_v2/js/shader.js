7f54e75a57ca355f85d7e994b5faca47eb9b2532
Murad Habib <murad.habib@arbisoft.com>
2019-03-10 18:45:15 +0500

    MCKIN-10066 fixed colors of engagement graph (#2677)
    Color fixed for engagement graph

--- static_v2/js/shader.js
+++ static_v2/js/shader.js
@@ -118,11 +118,10 @@ $(".ome").mouseenter(function (e) {
 });
 
 
-let color = $('.visualization .filled.total').css('background-color');
+let color = $('.visualization .filled.band-1').css('background-color');
 if (color !== undefined && color !== 'rgba(0, 0, 0, 0)') {
 
-    $('.visualization .filled.band-1').css('background-color', lighten(rgb2hex(color), 15));
-    $('.visualization .filled.band-2').css('background-color', lighten(rgb2hex(color), 30));
+    $('.visualization .filled.band-2').css('background-color', lighten(rgb2hex(color), 15));
 }
 
 if (typeof lighten !== "undefined" && primary != null) {
