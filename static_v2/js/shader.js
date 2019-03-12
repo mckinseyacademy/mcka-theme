d231341a7ec9ff1dfa00634f4be79c1ab38a03f4
Tanveer Ahmad <tanveer.ahmad@arbisoft.com>
2019-03-12 20:22:07 +0500

    style (#2690)

--- static_v2/js/shader.js
+++ static_v2/js/shader.js
@@ -126,8 +126,6 @@ if (color !== undefined && color !== 'rgba(0, 0, 0, 0)') {
 
 if (typeof lighten !== "undefined" && primary != null) {
     $('.bbGrid-container thead, .primaryBgLighten').css('background-color', lighten(primary, 60));
-
-    //  Using primary lighten color for description area of lessons page.
 }
 
 //  Course landing pagelesson bar hover bg color
