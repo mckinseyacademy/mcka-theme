58729eb962e1779da53280a5639320097ad4d39f
Hussnain Raza <hussnain.raza@arbisoft.com>
2019-03-27 23:09:25 +0500

    Address MCKIN-10225 and MCKIN-10226 in this commit. (#2784)
    Learner Dashboard navigation updates.

--- static_v2/js/course_learner_dashboard/utils.js
+++ static_v2/js/course_learner_dashboard/utils.js
@@ -15,28 +15,4 @@ $(function(){
 
    }
 
-
-   if ($(window).width() < 767) {
-       $('a.LDCourse').click(function (event) {
-           $(this).attr('href','#');
-           event.preventDefault();
-           if($('li.drop-open-dashboard')[0] != $(this).parent('li')[0])
-           {
-               $('li.drop-open-dashboard').children(":first").addClass('dome-c');
-               $('li.drop-open-dashboard').children(":first").css("color", primary);
-
-               $('li.drop-open-dashboard').removeClass();
-               $(this).parent('li').addClass('drop-open-dashboard');
-               $('li.drop-open-dashboard').children(":first").css("color","#000000");
-               $('li.drop-open-dashboard').children(":first").removeClass('dome-c');
-
-
-           }
-           else {
-               $('li.drop-open-dashboard').children(":first").css("color", primary);
-               $('li.drop-open-dashboard').children(":first").addClass('dome-c');
-               $('li.drop-open-dashboard').removeClass();
-           }
-       });
-   }
 });
