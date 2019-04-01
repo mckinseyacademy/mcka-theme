1b182dea09ae500fd6572fc3b13571198df475fa
Saqib <msaqib52@gmail.com>
2019-04-01 13:09:57 +0500

    reset checboxes and radio buttons on assessment try again (#78)

--- lms/static/js/apros-xblocks.js
+++ lms/static/js/apros-xblocks.js
@@ -3,7 +3,11 @@ $(function () {
   $(document).on('change', '.new-theme input[type=radio]', function(e) {
     var parent = $(e.target).parents('[data-block-type="pb-mcq"], [data-block-type="adventure"]');
     $(parent).find(".choice-selector").removeClass("selected");
-    $(e.target).parent().addClass("selected");
+    if (e.target.checked) {
+      $(e.target).parent().addClass("selected");
+    } else {
+      $(e.target).parent().removeClass("selected");
+    }
   });
 
   $(document).on('change', '.new-theme [data-block-type="pb-mrq"] input[type="checkbox"], .new-theme .post-options input[type="checkbox"]', function (e) {
@@ -17,13 +21,21 @@ $(function () {
   $(document).on('change', '.new-theme input[type=radio]', function (e) {
     var parent = $(e.target).parents('[data-block-type="poll"]');
     $(parent).find(".poll-input-container").removeClass("selected");
-    $(e.target).parent().addClass("selected");
+    if (e.target.checked) {
+      $(e.target).parent().addClass("selected");
+    } else {
+      $(e.target).parent().removeClass("selected");
+    }
   });
 
   $(document).on('change', '.new-theme input[type=radio]', function (e) {
     var parent = $(e.target).parents('.field-label, .survey-row');
     $(parent).find(".selected").removeClass("selected");
-    $(e.target).parent().addClass("selected");
+    if (e.target.checked) {
+      $(e.target).parent().addClass("selected");
+    } else {
+      $(e.target).parent().removeClass("selected");
+    }
   });
 
   // Add selected class to selected poll results when appended to DOM
