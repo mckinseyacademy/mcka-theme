4ff3e990fe43d9e93f0d1741c981cb5902fcaee9
prakashpathak02 <46211967+prakashpathak02@users.noreply.github.com>
2019-04-02 21:28:18 +0530

    mckin 10503 10527 except ie bugs  (#89)
    * Layout issues on mobile
    
    * adding disabled feature for radio buttons and applying it for survey - MCKIN-10527
    
    * username no link & spacing for mobile fixed
    
    * .row has been updated in UI fresh so synced new changes.
    
    * unused code remvoed
    
    * IE 11 - Grid not working
    
    * list of discussion width not set properly IE, pointer getting clicked in IE - fixed
    
    * IE select background & servey thank you message fixed

--- lms/static/js/apros-xblocks.js
+++ lms/static/js/apros-xblocks.js
@@ -48,6 +48,13 @@ $(function () {
     $(".new-theme .choices-list input[type=radio]:checked, .new-theme input[type=radio]:checked").parent().addClass('selected');
   });
 
+  // Survey block choice
+  $(document).on('DOMNodeInserted', '.survey-table', function(){
+    $(".new-theme input[type=radio]:checked").parent().addClass('selected');
+    $(".new-theme input[type=radio]").parent().addClass('disabled');
+  });
+
+
   // Assessment block checkbox
 
   $(document).on('DOMNodeInserted', '.choices-list, .choice-selector', function(){
