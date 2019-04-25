a392d774b9c2c0a0d5bf6e8ccb8296794fb4db09
Nasir Hussain <nasirhjafri@gmail.com>
2019-04-26 01:13:38 +0500

    MCKIN-10869 -- Added command to revert registration roles (#3025)

--- main/management/commands/update_registration_roles.py
+++ main/management/commands/update_registration_roles.py
@@ -6,5 +6,22 @@ from admin.models import SelfRegistrationRoles
 class Command(BaseCommand):
     help = 'Updates the Self registration roles'
 
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--revert', '-r', action='store_true', dest='revert'
+        )
+
     def handle(self, *args, **options):
-        SelfRegistrationRoles.objects.filter(option_text='Other (please describe below)').update(option_text='Other')
+        revert = options['revert']
+        if revert:
+            SelfRegistrationRoles.objects.filter(
+                option_text='Other'
+            ).update(
+                option_text='Other (please describe below)'
+            )
+        else:
+            SelfRegistrationRoles.objects.filter(
+                option_text='Other (please describe below)'
+            ).update(
+                option_text='Other'
+            )
