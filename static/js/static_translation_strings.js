03bb73de6dc80151aeed2ad5e8c78dc6179cd139
Moeez Zahid <moeezzahid1996@gmail.com>
2019-05-14 12:51:24 +0500

    MCKIN-10965 Add Scorm gating translations (#3100)

--- static/js/static_translation_strings.js
+++ static/js/static_translation_strings.js
@@ -2,3 +2,7 @@
 const scormPopupFailureMessageOne = gettext('It looks like your browser settings has pop-ups disabled.')
 const scormPopupFailureMessageTwo = gettext('The content takes place in a new window.')
 const scormPopupFailureButtonTitle = gettext('Launch pop-up to continue')
+// Location: edx-platform/xblocks/xblock-scorm/scormxblock/static/js/src/scormxblock.js
+const scormcompletedFeedbackText = gettext('Content is complete, please continue.');
+const scormIncompleteFeedbackText = gettext('Complete all content to continue.');
+const scormCompletionButtonTitle = gettext('Check for Completion');
