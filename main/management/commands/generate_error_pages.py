0e716c02564f9ecf86d14ad08631e969fab5261e
Saqib <msaqib52@gmail.com>
2019-05-31 18:29:57 +0500

    MCKIN-11076: serve i18n js file statically (#3166)
    * Revert "Revert "MCKIN-11076: serve i18n js file statically (#3161)" (#3165)"
    
    This reverts commit d1507b70109b512fd1df599c40e9219bcad9fab0.
    
    * fix generate_error_pages command to support statici18n
    
    * use LANGUAGE_CODE from settings

--- main/management/commands/generate_error_pages.py
+++ main/management/commands/generate_error_pages.py
@@ -1,7 +1,10 @@
 import codecs
 
+from django.conf import settings
 from django.core.management.base import BaseCommand
 from django.template.loader import render_to_string
+from django.utils import translation
+
 
 ERROR_PAGES = ['403', '404', '500', '503']
 
@@ -10,6 +13,7 @@ class Command(BaseCommand):
     help = 'Generates the static html error pages from the templates'
 
     def handle(self, *args, **options):
+        translation.activate(settings.LANGUAGE_CODE)
         for error_page in ERROR_PAGES:
             html_content = render_to_string('{}.haml'.format(error_page), {})
             with codecs.open(
@@ -18,3 +22,5 @@ class Command(BaseCommand):
                     encoding="utf-8"
             ) as static_error_page_file:
                 static_error_page_file.write(html_content)
+
+        translation.deactivate()
