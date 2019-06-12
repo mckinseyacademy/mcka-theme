b2c8b46009b6302bde8cef4afa9907c3bc1cbc00
Nasir Hussain <nasirhjafri@gmail.com>
2019-06-12 13:15:38 +0500

    MCKIN-10982 Removed spinner after first time (#3180)

--- static/js/views/admin/course_details.js
+++ static/js/views/admin/course_details.js
@@ -415,7 +415,6 @@
       var _this = event.data.extra;
       if  ($(this).find('.bbGrid-grid.table').height() - $(this).height() - $(this).scrollTop() < 20)
       {
-        $(this).find('i.fa-spinner').show();
         _this.coursesListDetailsViewGrid.partial_collection.getNextPage();
       }
     },
