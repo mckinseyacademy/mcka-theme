8b2ed423dea98da6cd9b8bdd38763343558da2e5
Naeem Ilyas <naeem-ilyas@live.com>
2019-06-13 15:12:57 +0500

    MCKIN-11013 - assets bundling and minification (#3175)
    * MCKIN-11013 - assets bundling and minification
    
    * bundled all js together

--- assets/assets_v2.py
+++ assets/assets_v2.py
@@ -21,6 +21,7 @@ js_files.extend([
     get_assets_path('js/vendor/moment-with-locales.min.js', v2=False),
     get_assets_path('js/vendor/moment-timezone.js', v2=False),
     get_assets_path('js/vendor/moment-timezone-with-data.js', v2=False),
+    get_assets_path('js/vendor/date.js', v2=False),
 
     get_assets_path('js/custom.js', v2=True),
     get_assets_path('js/common.js', v2=True),
@@ -31,6 +32,32 @@ js_files.extend([
     get_assets_path('js/router.js', v2=False),
     get_assets_path('js/config.js', v2=False),
     get_assets_path('js/utils.js', v2=False),
+
+    get_assets_path('js/css-vars-ponyfill.js', v2=True),
+    get_assets_path('js/vendor/jquery.cookie.js', v2=False),
+    get_assets_path('js/vendor/jquery.xblock.js', v2=False),
+    get_assets_path('js/bootstrap.bundle.min.js', v2=True),
+    get_assets_path('js/shader.js', v2=True),
+    get_assets_path('js/session_timeout_timer.js', v2=False),
+    get_assets_path('js/image_editor.js', v2=True),
+    get_assets_path('js/vendor/cropper.min.js', v2=False),
+    get_assets_path('js/user_profile_edit_v2.js', v2=True),
+
+    get_assets_path('js/ajaxify_overlay_form.js', v2=True),
+    get_assets_path('js/vendor/jquery.query-object.js', v2=True),
+    get_assets_path('js/accounts/user_login.js', v2=True),
+
+    get_assets_path('js/course_contact_modal.js', v2=False),
+    get_assets_path('js/course_learner_dashboard/tile_id_to_view.js', v2=False),
+    get_assets_path('js/course_learner_dashboard/learner_dashboard_calendar_load.js', v2=True),
+    get_assets_path('js/jquery.mCustomScrollbar.concat.min.js', v2=True),
+    get_assets_path('js/app_link_popup.js', v2=False),
+
+    get_assets_path('js/course_learner_dashboard/tile_progress_update.js', v2=True),
+    get_assets_path('js/course_lesson.js', v2=False),
+
+    get_assets_path('js/course_learner_dashboard/lesson_url_to_view.js', v2=True),
+    get_assets_path('js/course_contact.js', v2=True),
 ])
 
 js_files.extend(_build_file_list("js/common", ".js", v2=False))
@@ -38,7 +65,7 @@ js_files.extend(_build_file_list("js/models", ".js", v2=False))
 js_files.extend(_build_file_list("js/collections", ".js", v2=False))
 js_files.extend(_build_file_list("js/views", ".js", v2=False))
 
-# Javascript squashing
+# site-wide JS
 JS = Bundle(
     *js_files,
     filters='jsmin',
@@ -47,18 +74,19 @@ JS = Bundle(
 
 register('js_all_v2', JS)
 
-# CSS compilation and squashing
-# Core CSS
-
+# site-wide CSS
 SCSS_CORE_V2 = Bundle(
     get_assets_path('scss/core.scss', v2=True),
     filters='libsass',
     output='gen/core_v2.css',
 )
-register('scss_core_v2', SCSS_CORE_V2)
+core_css_files = [
+    SCSS_CORE_V2,
+    get_assets_path('css/cropper.css', v2=False),
+]
 
 CSS_CORE_V2 = Bundle(
-    SCSS_CORE_V2,
+    *core_css_files,
     filters='cssmin',
     output='gen/packed_core_v2.css'
 )
@@ -70,7 +98,6 @@ SCSS_LTR_V2 = Bundle(
     output='gen/ltr_v2.css',
     depends=('scss/**/*.scss')
 )
-register('scss_ltr_v2', SCSS_LTR_V2)
 
 CSS_LTR_V2 = Bundle(
     SCSS_LTR_V2,
@@ -85,7 +112,6 @@ SCSS_RTL_V2 = Bundle(
     output='gen/rtl_v2.css',
     depends=('scss/**/*.scss')
 )
-register('scss_rtl_v2', SCSS_RTL_V2)
 
 CSS_RTL_V2 = Bundle(
     SCSS_RTL_V2,
