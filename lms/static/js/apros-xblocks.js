3f17e95f8c5195b353c69c62eb8e4dc9fbe9dc51
shahidrafique <shahid.rafique@arbisoft.com>
2019-08-27 17:28:37 +0500

    MCKIN-10846 - fixed the sidebar issues of discussion. (#174)
    * MCKIN-10846 - fixed the sidebar isuses of discussion.

--- lms/static/js/apros-xblocks.js
+++ lms/static/js/apros-xblocks.js
@@ -1,6 +1,6 @@
 $(function () {
   var isSurveyTableFilled = false;
-  $(document).on('change', '.new-theme input[type=radio]', function(e) {
+  $(document).on('change', '.new-theme input[type=radio]', function (e) {
     var parent = $(e.target).parents('[data-block-type="pb-mcq"], [data-block-type="adventure"]');
     $(parent).find(".choice-selector").removeClass("selected");
     if (e.target.checked) {
@@ -39,28 +39,70 @@ $(function () {
   });
 
   // Add selected class to selected poll results when appended to DOM
-  $(document).on('DOMNodeInserted', '.poll-results-wrapper, .lessonOverview, .forum-new-post-form, .edit-post-form', function(){
+  $(document).on('DOMNodeInserted', '.poll-results-wrapper, .lessonOverview, .forum-new-post-form, .edit-post-form', function () {
     $(".new-theme .poll-results input[type=radio]:checked, .new-theme input[type=radio]:checked").parent().addClass('selected');
     $(".new-theme .poll-results input[type=radio]:disabled, .new-theme input[type=radio]:disabled").parent().addClass('disabled');
 
     //fall back for private results where results are fetched after 1-2 seconds
-    setTimeout(function(){
+    setTimeout(function () {
       $(".new-theme input[type=radio]:disabled").parent().addClass('disabled');
-    },3000);
+    }, 3000);
 
     //discussion inline- big heading
-    if($(this).find('.discussion-module-header').length > 0) {
+    if ($(this).find('.discussion-module-header').length > 0) {
       inlineDiscussionLongHeading();
     }
   });
 
+  // Discussion sticky stopper handling
+  $(document).on("DOMNodeInserted", '.new-theme .forum-nav', function(event) {
+    let $sticky = $('.forum-nav');
+    let $stickyrStopper = $('.sticky-stopper');
+    let windowWidth = window.innerWidth;
+    if (windowWidth > 1024) {
+      if (!!$sticky.offset()) { // make sure ".sticky" element exists
+        let generalSidebarHeight = $sticky.innerHeight();
+        let stickyTop = $sticky.offset().top;
+        let stickOffset = 30;
+        let stickyStopperPosition = $stickyrStopper.offset().top;
+        let stopPoint = stickyStopperPosition - generalSidebarHeight + 10000;
+        let diff = stopPoint + stickOffset;
+
+        $(window).scroll(function () { // scroll event
+          let windowTop = $(window).scrollTop(); // returns number
+
+          if (stopPoint < windowTop) {
+            $sticky.css({
+              position: 'absolute',
+              top: diff,
+              height: "calc(100vh  -  396px)",
+            });
+          } else if (stickyTop < windowTop + stickOffset) {
+            $sticky.css({
+              position: 'fixed',
+              top: stickOffset,
+              height: "calc(100vh  -  100px)",
+            });
+          } else {
+            $sticky.css({
+              position: 'absolute',
+              top: 'initial',
+              height: "calc(100vh  -  396px)",
+            });
+          }
+        });
+
+      }
+    }
+  });
+
   // assessment block choice
-  $(document).on('DOMNodeInserted', '.choices-list, .choice, .choice-selector', function(){
+  $(document).on('DOMNodeInserted', '.choices-list, .choice, .choice-selector', function () {
     $(".new-theme .choices-list input[type=radio]:checked, .new-theme input[type=radio]:checked").parent().addClass('selected');
   });
-  $(document).on('DOMNodeInserted', '.image-explorer-wrapper', function(){
-    $('.image-explorer-hotspot-reveal-body > div').each(function(index, obj){
-      if(!$(this).attr('id') || $(this).attr('id').indexOf('ooyala') === -1) {
+  $(document).on('DOMNodeInserted', '.image-explorer-wrapper', function () {
+    $('.image-explorer-hotspot-reveal-body > div').each(function (index, obj) {
+      if (!$(this).attr('id') || $(this).attr('id').indexOf('ooyala') === -1) {
         $(this).addClass('ie-custom-div')
       }
     })
@@ -68,33 +110,33 @@ $(function () {
 
   // Assessment block checkbox
 
-  $(document).on('DOMNodeInserted', '.choices-list, .choice-selector', function(){
+  $(document).on('DOMNodeInserted', '.choices-list, .choice-selector', function () {
     $(".new-theme .choice-selector input[type=checkbox]:checked").parent().addClass('selected');
   });
 
-  $(document).on('DOMNodeInserted', '.forum-new-post-form', function(){
+  $(document).on('DOMNodeInserted', '.forum-new-post-form', function () {
     $(".new-theme input[type=checkbox]:checked").parent().addClass('selected');
   });
 
-  $(document).on('DOMNodeInserted', '.new-theme .lesson-content', function(){
-    if(!isSurveyTableFilled) {
+  $(document).on('DOMNodeInserted', '.new-theme .lesson-content', function () {
+    if (!isSurveyTableFilled) {
       isSurveyTableFilled = true;
-      setTimeout(function(){
+      setTimeout(function () {
         surveyTableLabelPositionsForMobile();
       }, 3000);
     }
   });
 });
 
-function surveyTableLabelPositionsForMobile(){
-  $('.new-theme .survey-table .survey-option').each(function(index, element){
+function surveyTableLabelPositionsForMobile() {
+  $('.new-theme .survey-table .survey-option').each(function (index, element) {
     var span = $(element).find('.visible-mobile-only').prop('outerHTML');
     $(element).find('.visible-mobile-only').remove();
     $(element).append(span);
     if (index == $('.survey-table .survey-option').length - 1) {
       setTimeout(function () {
         toggleSurveyRadios()
-      }, 5000);  
+      }, 5000);
     }
   });
 }
@@ -107,7 +149,7 @@ function toggleSurveyRadios() {
       $(el).parent().addClass('disabled');
   });
   $('.new-theme [data-block-type="survey"] input[type=button]').on('click', function (e) {
-    // Survey block choice 
+    // Survey block choice
     setTimeout(function () { // we have to wait till api responses and updates selected/disabled attributes to input
       $(e.target).prev().find('input[type=radio]').each(function (index, el) {
         if (el.checked)
@@ -122,15 +164,19 @@ function toggleSurveyRadios() {
 
 function inlineDiscussionLongHeading() {
   var moduleWidth = $('.discussion-module-header').width();
-  $('.discussion-module-header').each(function(){
-    $(this).children('.discussion-module-title').css({'display': 'inline'}); // to get actual width
+  $('.discussion-module-header').each(function () {
+    $(this).children('.discussion-module-title').css({
+      'display': 'inline'
+    }); // to get actual width
     //if width is more than 65 % move it downwards
-    if($(this).children('.discussion-module-title').eq(0).width() / moduleWidth  * 100 > 65){
+    if ($(this).children('.discussion-module-title').eq(0).width() / moduleWidth * 100 > 65) {
       $(this).parent('.discussion-module').addClass('long-discussion-heading');
     }
     // else // We will enable for testing, its not required for production.
     //   $(this).parent('.discussion-module').removeClass('long-discussion-heading');
-    $(this).children('.discussion-module-title').css({'display': ''}); // rest width
+    $(this).children('.discussion-module-title').css({
+      'display': ''
+    }); // rest width
   });
 }
 // We will enable for testing, its not required for production.
