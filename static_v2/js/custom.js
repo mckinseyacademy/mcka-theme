8a5e96d5d5b604ada2586801fa0f8ba724b8681c
moeez96 <moeezzahid1996@gmail.com>
2019-10-07 18:57:31 +0500

    Revert "MCKIN-11453 ooyala fullscreen for react native (#3407)"
    This reverts commit 3ac0fa81ab64d2866839e859ab9cf4d8a8ffe3aa.

--- static_v2/js/custom.js
+++ static_v2/js/custom.js
@@ -306,10 +306,6 @@ $(document).ready(function () {
     if ($(window).width() <= 1024 && $('.progress-data tr td').length >= 2) {
         $(".progress-data").addClass("three-cols");
     }
-
-    $('.lesson-content').on('click', '.oo-fullscreen', function () {
-        $('#moduleNavigator').toggleClass('full-screen-mode');
-    });
 });
 
 
@@ -676,10 +672,6 @@ function scaleImage(label) {
         $(label).find("img").css("height", imgHeight)
         $(label).find("img").css("width", "auto")
     }
-}
-
-function removeCardLoader(image) {
-    $(image).parents('.course-card').removeClass('skeleton-loading')
 }
 
 $(window).resize(function () {
