8fcbf6deddb8e667451574e94de393fd6fc876da
Kshitij Sobti <kshitij@sobti.in>
2019-10-30 13:45:03 +0000

    [MCKIN-12585]  Add logging to clearsessions_batch (#3445)

--- main/management/commands/clearsessions_batch.py
+++ main/management/commands/clearsessions_batch.py
@@ -23,13 +23,17 @@ class Command(BaseCommand):
         )
 
     def handle(self, *args, **options):
+        logger.info('Got batch size of %s', options['batch_size'])
         session_ids = Session.objects.filter(
             expire_date__lt=timezone.now()
         )[:options['batch_size']].values_list("session_key", flat=True)
-        delete_count, _ = Session.objects.filter(pk__in=list(session_ids)).delete()
+        logger.info('Collected %s session ids', len(session_ids))
+        sessions_to_delete = Session.objects.filter(pk__in=list(session_ids))
+        logger.info('Collected %s sessions to delete', sessions_to_delete.count())
+        delete_count, _ = sessions_to_delete.delete()
 
         logger.info("Cleared %s sessions", delete_count)
 
         if delete_count < options['batch_size']:
-            # Raise error so we know when to switch to the regular clearsessions command
+            # Print warning so we know when to switch to the regular clearsessions command
             logger.warn("No more sessions to clear")
--- main/tests/test_management_clearsessions_batch.py
+++ main/tests/test_management_clearsessions_batch.py
@@ -4,7 +4,7 @@ from django.contrib.sessions.models import Session
 from django.test import TestCase, override_settings
 from django.utils import timezone
 from freezegun import freeze_time
-from mock import patch
+from mock import patch, call
 
 
 @override_settings(SESSION_ENGINE='django.contrib.sessions.backends.db')
@@ -27,13 +27,26 @@ class ClearSessionsBarchCommandTestCase(TestCase):
 
     def test_command(self):
         self.assertEqual(Session.objects.all().count(), 90)
-        # Running it the first 6 times should clear 10 sessions each time
-        for run_count in range(1, 7):
+        with patch('main.management.commands.clearsessions_batch.logger') as mock_log:
+            # Running it the first 6 times should clear 10 sessions each time
+            for run_count in range(1, 7):
+                call_command('clearsessions_batch', '10')
+                mock_log.info.assert_has_calls([
+                    call('Got batch size of %s', 10),
+                    call('Collected %s session ids', 10),
+                    call('Collected %s sessions to delete', 10),
+                    call('Cleared %s sessions', 10),
+                ])
+                self.assertEqual(Session.objects.all().count(), 90 - 10 * run_count)
+                mock_log.reset_mock()
+            # The run after that should only clear 1 expired session
             call_command('clearsessions_batch', '10')
-            self.assertEqual(Session.objects.all().count(), 90 - 10 * run_count)
-        # The run after that should only clear 1 expired session
-        with patch('main.management.commands.clearsessions_batch.logger') as mock:
-            call_command('clearsessions_batch', '10')
-            mock.warn.assert_called_with("No more sessions to clear")
+            mock_log.info.assert_has_calls([
+                call('Got batch size of %s', 10),
+                call('Collected %s session ids', 0),
+                call('Collected %s sessions to delete', 0),
+                call('Cleared %s sessions', 0),
+            ])
+            mock_log.warn.assert_called_with("No more sessions to clear")
 
         self.assertEqual(Session.objects.all().count(), 30)
