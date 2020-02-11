70b124420f81a095c560994b6ab5d8ba3b280ea1
Muhammad Usman <m.usman@arbisoft.com>
2020-02-11 12:14:24 +0500

    duplicate code removed

--- lms/static/js/apros-xblocks.js
+++ lms/static/js/apros-xblocks.js
@@ -208,10 +208,3 @@ function inlineDiscussionLongHeading() {
     }); // rest width
   });
 }
-
-$(document).on('click', '.discussion-show.btn', function() {
-  if ($('.add_post_btn_container').hasClass('is-hidden') && $(this).hasClass('shown') )
-  {
-    $('.thread-title').focus()
-  }
-});
