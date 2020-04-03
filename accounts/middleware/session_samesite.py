c2d7f05996a4dff9a579c717baf591ff8cd3b68f
Moeez Zahid <moeezzahid1996@gmail.com>
2020-04-03 15:58:28 +0500

    MCKIN-14919 Allow session cookie cross origin share (#3693)

--- accounts/middleware/session_samesite.py
+++ accounts/middleware/session_samesite.py
@@ -6,9 +6,12 @@ class SessionMiddleware(SessionMiddleware):
     def process_response(self, request, response):
         response = super(SessionMiddleware, self).process_response(request, response)
 
-        if settings.SESSION_COOKIE_NAME in response.cookies:
-            response.cookies[settings.SESSION_COOKIE_NAME]['samesite'] = 'None'
-        if settings.CSRF_COOKIE_NAME in response.cookies:
-            response.cookies[settings.CSRF_COOKIE_NAME]['samesite'] = 'None'
+        if getattr(settings, 'ENVIRONMENT', '') != 'development':
+            if settings.SESSION_COOKIE_NAME in response.cookies:
+                response.cookies[settings.SESSION_COOKIE_NAME]['samesite'] = 'None'
+                response.cookies[settings.SESSION_COOKIE_NAME]['secure'] = True
+            if 'sessionid' in response.cookies:
+                response.cookies['sessionid']['samesite'] = 'None'
+                response.cookies['sessionid']['secure'] = True
 
         return response
