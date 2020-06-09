8b6671728c4801636be39725fce049fbe6f2df77
Hussnain Raza <hussnain.raza@arbisoft.com>
2020-06-09 17:00:59 +0500

    Revert "Mckin 18290 popover (#354)"
    This reverts commit 55fe376b061e918ff5547fe48e0ace3f9122d368.

--- lms/static/js/apros-xblocks.js
+++ lms/static/js/apros-xblocks.js
@@ -142,14 +142,6 @@ $(function () {
                 $(this).siblings('.image-explorer-hotspot-reveal').css({"bottom": '0', "top": "auto"});
             }
         });
-
-          //popover positioning for MRQ.MCQ when popover height bigger.
-          $(function (e) {
-              var popHeight = $(".choice-tips-container.with-tips.active .choice-tips").outerHeight();
-              if (popHeight > 320) {
-                  $(".choice-tips").parent().addClass('toTop');
-              }
-          });
     });
 
   // Assessment block checkbox
